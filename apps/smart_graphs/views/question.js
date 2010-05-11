c305a4759e56999851dc6a0376d4793e3dc4a242
Richard Klancer <rpk@pobox.com>
2010-05-11 12:41:48 -0400

    Improved responses to student input
    Hid "null" messages.
    Some code clean-up
    TODO: Move business logic from QuestionView and put it in some SmartGraphs controlller

--- apps/smart_graphs/views/question.js
+++ apps/smart_graphs/views/question.js
@@ -14,20 +14,18 @@
 sc_require('main');
 
 SmartGraphs.QuestionView = SC.View.extend({
-	//layout: {top: 20, left: 20, right: 0},
-	
+  //layout: {top: 20, left: 20, right: 0},
   contentDisplayProperties: 'prompt'.w(),
-	prompt: "[prompt]",
-	
-  inputType: SmartGraphs.TEXT_RESPONSE,      // default; should migrate this to a controller
+  prompt: "[prompt]",
 
-  textInputShouldBeVisibleBinding: SC.Binding.transform( function (value) {
+  inputType: SmartGraphs.TEXT_RESPONSE,
+  // default; should migrate this to a controller
+  textInputShouldBeVisibleBinding: SC.Binding.transform(function(value) {
     return (value === SmartGraphs.TEXT_RESPONSE);
   }).from('.inputType'),
 
   // FIXME this should migrate to a controller, of course!
-  
-  answer: function () {
+  answer: function() {
     var inputType = this.get('inputType');
 
     if (inputType === SmartGraphs.TEXT_RESPONSE) {
@@ -41,41 +39,47 @@ SmartGraphs.QuestionView = SC.View.extend({
       console.log('selection = ' + selection);
       return (selection.get('length') === 1) ? selection.toArray().objectAt(0).get('x') : null;
     }
-  }.property('.inputType', '.inputView.textFieldView.value', 'SmartGraphs.dataSeriesController.selection'),
-  
-	childViews: 'promptView inputView checkButton'.w(),
-	
-	promptView: SC.StaticContentView.design({
-		contentBinding: ".parentView.prompt"
-	}),
+  }.property(),
 
-  inputView: SC.View.design({
-   layout: { height: 20, width: 150 },
-   useStaticLayout: YES,
-   isVisibleBinding: '.parentView.textInputShouldBeVisible',
-   childViews: 'textFieldView'.w(),
-   textFieldView: SC.TextFieldView.design({
-     //classNames: 'question-input',
-     isTextArea: YES
-   })
+  childViews: 'promptView inputView checkButton'.w(),
+
+  promptView: SC.StaticContentView.design({
+    contentBinding: ".parentView.prompt"
   }),
 
-	checkButton: SC.ButtonView.design({
+  inputView: SC.View.design({
+    layout: {
+      height: 20,
+      width: 150
+    },
     useStaticLayout: YES,
-	  //layout: { top: 10, left: 20, width: 120 },
-	  title: "Check Answer",
+    isVisibleBinding: '.parentView.textInputShouldBeVisible',
+    childViews: 'textFieldView'.w(),
+    textFieldView: SC.TextFieldView.design({
+      //classNames: 'question-input',
+      isTextArea: YES
+    })
+  }),
+
+  checkButton: SC.ButtonView.design({
+    useStaticLayout: YES,
+    //layout: { top: 10, left: 20, width: 120 },
+    title: "Check Answer",
     //answerBinding: '.parentView.answer',
-    
-	  action: function () {
-	    var answer = this.getPath('parentView.answer');
-	    
-	    //handle empty answers.
-	    if (answer === this.getPath('parentView.correctAnswer')) {
-	      SC.AlertPane.info("'" + answer + "' is the correct answer!");
-	    }
-	    else {
-	      SC.AlertPane.info("Sorry, '" + answer + "' is not the correct answer.");
-	    }
-	  }
-	})
+    action: function() {
+      var answer = this.getPath('parentView.answer');
+
+      //handle empty answers.
+      if (answer) {
+        if (answer === this.getPath('parentView.correctAnswer')) {
+          SC.AlertPane.info("'" + answer + "' is the correct answer!");
+        }
+        else {
+          SC.AlertPane.info("Sorry, '" + answer + "' is not the correct answer.");
+        }
+      } else {
+        SC.AlertPane.info("Sorry, that is not the correct answer.");
+      }
+    }
+  })
 });
