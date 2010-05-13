a090847bd514037dbd974e5a20339c83731ba0b3
Richard Klancer <rpk@pobox.com>
2010-05-13 17:45:30 -0400

    Implemented tests for text response checking in questionController

--- apps/smart_graphs/tests/controllers/question.js
+++ apps/smart_graphs/tests/controllers/question.js
@@ -4,12 +4,33 @@
 // ==========================================================================
 /*globals SmartGraphs module test ok equals same stop start */
 
-module("SmartGraphs.questionController");
-
-// TODO: Replace with real unit test for SmartGraphs.questionController
-test("test description", function() {
-  var expected = "test";
-  var result   = "test";
-  equals(result, expected, "test should equal test");
+module("SmartGraphs.questionController", {
+  setup: function () {
+    textQ = SmartGraphs.Question.create({ 
+      responseType: SmartGraphs.TEXT_RESPONSE,
+      prompt: '[prompt]',
+      correctResponse: '[correct-response]',
+      correctResponseFeedback: '[correct-feedback]',
+      incorrectResponseFeedback: '[incorrect-feedback]'
+    });
+  }
 });
 
+// TODO: Replace with real unit test for SmartGraphs.questionController
+test("checkResponse should work correctly with text response", function() {
+  var qc = SmartGraphs.questionController;
+  qc.set('content', textQ);
+  qc.set('textResponse', '[correct-response]');
+  
+  var feedback = qc.get('feedback');
+  ok(!feedback, 'Feedback string should be falsy before checkResponse() is called');
+  
+  qc.checkResponse();
+  feedback = qc.get('feedback');
+  equals(feedback, '[correct-feedback]', 'feedback property after checkResponse() on an correct response should be appropriate');
+  
+  qc.set('textResponse', '[incorrect-response]');
+  qc.checkResponse();
+  feedback = qc.get('feedback');
+  equals(feedback, '[incorrect-feedback]', 'feedback property after checkResponse() on an incorrect response should be appropriate');
+});
