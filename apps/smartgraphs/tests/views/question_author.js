94c32cbca19acd67d1738055c1d7a3861cb858f3
Baba Kofi Weusijana <kweusijana@asset-infoserv-loaner.local>
2010-06-02 12:55:48 -0700

    Renamed QuestionAuthorView to AuthorView

--- apps/smartgraphs/tests/views/question_author.js
+++ apps/smartgraphs/tests/views/question_author.js
@@ -1,12 +1,12 @@
 // ==========================================================================
-// Project:   Smartgraphs.QuestionAuthorView Unit Test
+// Project:   Smartgraphs.AuthorView Unit Test
 // Copyright: ©2010 Concord Consortium
 // ==========================================================================
 /*globals Smartgraphs module test ok equals same stop start */
 
-module("Smartgraphs.QuestionAuthorView");
+module("Smartgraphs.AuthorView");
 
-// TODO: Replace with real unit test for Smartgraphs.QuestionAuthorView
+// TODO: Replace with real unit test for Smartgraphs.AuthorView
 test("test description", function() {
   var expected = "test";
   var result   = "test";
