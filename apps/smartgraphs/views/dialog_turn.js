d01d6fdeb55e5beda2eb02ddf797a41205f854b5
Richard Klancer <rpk@pobox.com>
2010-06-03 12:36:48 -0400

    Begin style fixup of DialogTurn.

--- apps/smartgraphs/views/dialog_turn.js
+++ apps/smartgraphs/views/dialog_turn.js
@@ -15,90 +15,100 @@ Smartgraphs.DialogTurnView = SC.View.extend(
   
   useStaticLayout: YES,
   
-  childViews: 'beforeTextView responseFieldsView afterTextView buttonsView'.w(),
+  childViews: 'textView buttonsView'.w(),
   
-  beforeTextView: SC.StaticContentView.design({
-    contentBinding: SC.Binding.oneWay('Smartgraphs.dialogTurnController.beforeText'),
-    isVisibleBinding: SC.Binding.bool('Smartgraphs.dialogTurnController.beforeText')
-  }),
+  textView: SC.View.extend( {
+    useStaticLayout: YES,
+    
+    childViews: 'beforeTextView responseFieldsView afterTextView'.w(),
+    
+    classNames: 'dialog-turn'.w(),
   
-  // FIXME: this could go into its own view class.
-  // responseFields could be a property on the responseTemplateController, bound to dialogTurnView
-  // (responses do properly live with the DialogTurn...)
+    beforeTextView: SC.StaticContentView.design({
+      contentBinding: SC.Binding.oneWay('Smartgraphs.dialogTurnController.beforeText'),
+      isVisibleBinding: SC.Binding.bool('Smartgraphs.dialogTurnController.beforeText')
+    }),
   
-  responseFieldsView: SC.StaticContentView.design({
+    // FIXME: this could go into its own view class.
+    // responseFields could be a property on the responseTemplateController, bound to dialogTurnView
+    // (responses do properly live with the DialogTurn...)
+  
+    responseFieldsView: SC.StaticContentView.design({
     
-    fieldTypesBinding: 'Smartgraphs.responseTemplateController.fieldTypes',
-    fieldValuesBinding: 'Smartgraphs.responseTemplateController.fieldValues',
+      fieldTypesBinding: 'Smartgraphs.responseTemplateController.fieldTypes',
+      fieldValuesBinding: 'Smartgraphs.responseTemplateController.fieldValues',
     
-    fieldsTypesDidChange: function () {
-      //console.log('fieldTypesDidChange');
-      this.invokeOnce(this._updateChildViews);
-    }.observes('fieldTypes'),
+      fieldsTypesDidChange: function () {
+        //console.log('fieldTypesDidChange');
+        this.invokeOnce(this._updateChildViews);
+      }.observes('fieldTypes'),
     
-    fieldValuesDidChange: function () {
-      //console.log('fieldValuesDidChange');      
-      this.invokeOnce(this._updateChildViews);
-    }.observes('fieldValues'),
+      fieldValuesDidChange: function () {
+        //console.log('fieldValuesDidChange');      
+        this.invokeOnce(this._updateChildViews);
+      }.observes('fieldValues'),
     
-    _updateChildViews: function () {
-      //console.log('_updateChildViews');   
-      this.removeAllChildren();
-      this.contentLayoutDidChange();
+      _updateChildViews: function () {
+        //console.log('_updateChildViews');   
+        this.removeAllChildren();
+        this.contentLayoutDidChange();
 
-      var fieldTypes = this.get('fieldTypes');
-      var fieldValues = this.get('fieldValues');
+        var fieldTypes = this.get('fieldTypes');
+        var fieldValues = this.get('fieldValues');
       
-      if (!fieldTypes) {
-        return;
-      }
+        if (!fieldTypes) {
+          return;
+        }
 
-      var type, viewDesign, isTextArea, layout;
+        var type, viewDesign, isTextArea, layout, hint = '';
       
-      for (var i = 0, ii = fieldTypes.get('length'); i < ii; i++) {
-        type = fieldTypes.objectAt(i);
+        for (var i = 0, ii = fieldTypes.get('length'); i < ii; i++) {
+          type = fieldTypes.objectAt(i);
         
-        if (type === 'textarea') {
-          isTextArea = YES;
-          layout = {
-            height: 110
-          };
-        }
-        else if (type === 'numeric') {
-          isTextArea = NO;
-          layout = {
-            height: 22,
-            width: 100
-          };          
-        }
-        else {
-          throw "responseFieldsView received unexpected field type string '" + type + "'.";
-        }
+          if (type === 'textarea') {
+            isTextArea = YES;
+            hint = 'Enter you answer here...';
+            layout = {
+              height: 97
+            };
+          }
+          else if (type === 'numeric') {
+            isTextArea = NO;
+            layout = {
+              height: 22,
+              width: 100 
+            };          
+          }
+          else {
+            throw "responseFieldsView received unexpected field type string '" + type + "'.";
+          }
 
-        viewDesign = SC.View.design({
-          useStaticLayout: YES,
-          layout: layout,
+          viewDesign = SC.View.design({
+            useStaticLayout: YES,
+            layout: layout,
 
-          childViews: [SC.TextFieldView.design({
-            isTextArea: isTextArea,
-            index: i,
-            valueDidChange: function () {
-              //console.log("responseFieldsView's child textFieldView observed value");
-              var index = this.get('index');
-              Smartgraphs.dialogTurnController.updateResponse(index, this.get('value'));
-            }.observes('value')
-          })]
-        });
-        this.appendChild(viewDesign.create());
-      }
+            childViews: [SC.TextFieldView.design({
+              isTextArea: isTextArea,
+              hint: hint,
+              index: i,
+              valueDidChange: function () {
+                //console.log("responseFieldsView's child textFieldView observed value");
+                var index = this.get('index');
+                Smartgraphs.dialogTurnController.updateResponse(index, this.get('value'));
+              }.observes('value')
+            })]
+          });
+          this.appendChild(viewDesign.create());
+        }
       
-      this.contentLayoutDidChange();
-    }
-  }),
+        this.contentLayoutDidChange();
+      }
+    }),
   
-  afterTextView: SC.StaticContentView.design({
-    contentBinding: SC.Binding.oneWay('Smartgraphs.dialogTurnController.afterText'),
-    isVisibleBinding: SC.Binding.bool('Smartgraphs.dialogTurnController.afterText')
+    afterTextView: SC.StaticContentView.design({
+      contentBinding: SC.Binding.oneWay('Smartgraphs.dialogTurnController.afterText'),
+      isVisibleBinding: SC.Binding.bool('Smartgraphs.dialogTurnController.afterText')
+    })
   }),
   
   buttonsView: SC.View.design({
