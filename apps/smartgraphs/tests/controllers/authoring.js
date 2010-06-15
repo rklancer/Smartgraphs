671f9d8700b6edbeb90f16798a6312fda531316a
Baba Kofi Weusijana <kweusijana@asset-infoserv-loaner.local>
2010-06-15 15:04:07 -0700

    Added unit test file for authoring controller

---
+++ apps/smartgraphs/tests/controllers/authoring.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Smartgraphs Unit Test
+// Copyright: ©2010 Concord Consortium
+// ==========================================================================
+/*globals Smartgraphs module test ok equals same stop start */
+
+module("Smartgraphs.authoringController");
+
+// TODO: Replace with real unit test
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
