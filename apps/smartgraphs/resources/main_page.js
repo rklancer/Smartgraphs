3f5d2b0e966f7197adb1be797aa4ef0f67049ff5
Richard Klancer <rpk@pobox.com>
2010-06-17 21:35:58 -0400

    Removed 'EditableListItemView' which was a fix for a now-outdated SC bug.

--- apps/smartgraphs/resources/main_page.js
+++ apps/smartgraphs/resources/main_page.js
@@ -246,8 +246,6 @@ Smartgraphs.mainPage = SC.Page.design({
             },
 
             canEditContent: NO,
-            // as per http://groups.google.com/group/sproutcore/browse_thread/thread/6564941be2b51276/fcf4eb11a1ea268f?#fcf4eb11a1ea268f
-            exampleView: Smartgraphs.EditableListItemView,
             contentValueKey: 'y',
             contentBinding: 'Smartgraphs.dataSeriesController.arrangedObjects',
             selectionBinding: 'Smartgraphs.dataSeriesController.selection',
