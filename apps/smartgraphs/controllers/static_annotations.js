b16aaca970284ecda4c382541a186176099d084f
Richard Klancer <rpk@pobox.com>
2010-06-22 23:13:39 -0400

    Comment fixes.

--- apps/smartgraphs/controllers/static_annotations.js
+++ apps/smartgraphs/controllers/static_annotations.js
@@ -1,6 +1,6 @@
 // ==========================================================================
 // Project:   Smartgraphs.staticAnnotationsController
-// Copyright: ©2010 My Company, Inc.
+// Copyright: ©2010 Concord Consortium, Inc.
 // ==========================================================================
 /*globals Smartgraphs */
 
