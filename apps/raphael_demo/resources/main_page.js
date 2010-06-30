3bb0b5798681fcb2b8fc75faa7d1d756f9abab91
Richard Klancer <rpk@pobox.com>
2010-06-30 15:20:32 -0400

    Created RaphaelDemo app

---
+++ apps/raphael_demo/resources/main_page.js
@@ -0,0 +1,24 @@
+// ==========================================================================
+// Project:   RaphaelDemo - mainPage
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals RaphaelDemo */
+
+// This page describes the main user interface for your application.  
+RaphaelDemo.mainPage = SC.Page.design({
+
+  // The main pane is made visible on screen as soon as your app is loaded.
+  // Add childViews to this pane for views to display immediately on page 
+  // load.
+  mainPane: SC.MainPane.design({
+    childViews: 'labelView'.w(),
+    
+    labelView: SC.LabelView.design({
+      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
+      textAlign: SC.ALIGN_CENTER,
+      tagName: "h1", 
+      value: "Welcome to SproutCore!"
+    })
+  })
+
+});
