46d7f687ae2a3ebd85544361224f1f5a2d246459
Baba Kofi Weusijana <kweusijana@asset-infoserv-loaner.local>
2010-07-14 12:06:36 -0700

    Updated authoring tests so password dialog doesn't show

--- apps/smartgraphs/tests/controllers/authoring.js
+++ apps/smartgraphs/tests/controllers/authoring.js
@@ -5,25 +5,26 @@
 /*globals Smartgraphs module test ok equals same stop start */
 
 module("Smartgraphs.authoringController", {
-  setup: function(){
-    // ... perform setup code here
-    console.log("setup complete for Smartgraphs.authoringController test module");
-  },
-  
-  teardown: function(){
-    // ... perform teardown code here
-    console.log("teardown complete for Smartgraphs.authoringController test module");
-  }
+    setup: function() {
+        // ... perform setup code here
+        console.log("setup complete for Smartgraphs.authoringController test module");
+    },
+
+    teardown: function() {
+        // ... perform teardown code here
+        console.log("teardown complete for Smartgraphs.authoringController test module");
+    }
 });
 
 // .. unit tests go here
-test("toggleAuthoring function should toggle isAuthoring boolean", function(){
-  var isAuthoring1 = Smartgraphs.authoringController.get('isAuthoring');
-  var toggleAuthoringResult1 = Smartgraphs.authoringController.toggleAuthoring();
-  var isAuthoring2 = Smartgraphs.authoringController.get('isAuthoring');
-  var toggleAuthoringResult2 = Smartgraphs.authoringController.toggleAuthoring();
-  var isAuthoring3 = Smartgraphs.authoringController.get('isAuthoring');
-  equals(isAuthoring1, false, "isAuthoring should start false");
-  equals(isAuthoring1, !isAuthoring2, "isAuthoring should be true after one call to toggleAuthoring()");
-  equals(isAuthoring1, isAuthoring3, "isAuthoring should be false after two calls to toggleAuthoring()");
+test("toggleAuthoringMode function should toggle isAuthoring boolean",
+function() {
+    var isAuthoring1 = Smartgraphs.authoringController.get('isAuthoring');
+    var toggleAuthoringModeResult1 = Smartgraphs.authoringController.toggleAuthoringMode();
+    var isAuthoring2 = Smartgraphs.authoringController.get('isAuthoring');
+    var toggleAuthoringModeResult2 = Smartgraphs.authoringController.toggleAuthoringMode();
+    var isAuthoring3 = Smartgraphs.authoringController.get('isAuthoring');
+    equals(isAuthoring1, false, "isAuthoring should start false");
+    equals(isAuthoring1, !isAuthoring2, "isAuthoring should be true after one call to toggleAuthoringMode()");
+    equals(isAuthoring1, isAuthoring3, "isAuthoring should be false after two calls to toggleAuthoringMode()");
 });
