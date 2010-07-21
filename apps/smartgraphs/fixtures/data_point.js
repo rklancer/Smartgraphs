334e616428d642398001887778b03e26b9eba714
Richard Klancer <rpk@pobox.com>
2010-07-21 19:45:41 -0400

    DataPoint fixtures for testing GraphView

--- apps/smartgraphs/fixtures/data_point.js
+++ apps/smartgraphs/fixtures/data_point.js
@@ -8,4 +8,16 @@ sc_require('models/data_point');
 
 Smartgraphs.DataPoint.FIXTURES = [
 
+  {x: 1,  y: 1, guid: 1,  series: 'prediction-away-series'},
+  {x: 4,  y: 2, guid: 2,  series: 'prediction-away-series'},
+  {x: 9,  y: 3, guid: 3,  series: 'prediction-away-series'},
+  {x: 12, y: 4, guid: 4,  series: 'prediction-away-series'},
+  {x: 15, y: 5, guid: 5,  series: 'prediction-away-series'},
+  
+  {x: 0,  y: 5, guid: 6,  series: 'prediction-toward-series'},
+  {x: 3,  y: 4, guid: 7,  series: 'prediction-toward-series'},
+  {x: 6,  y: 3, guid: 8,  series: 'prediction-toward-series'},
+  {x: 9,  y: 2, guid: 9,  series: 'prediction-toward-series'},
+  {x: 12, y: 1, guid: 10, series: 'prediction-toward-series'}
+
 ];
