56dbdc17cdfe1785ff866e1716bfbbeea99f6312
Richard Klancer <rpk@pobox.com>
2010-08-07 01:59:52 -0400

    Comment out fixtures data now moved to server

--- apps/smartgraphs/data_sources/mock_responses/activity.js
+++ apps/smartgraphs/data_sources/mock_responses/activity.js
@@ -7,26 +7,26 @@
 
 sc_require('data_sources/mock_responses/mock_responses');
 
-Smartgraphs.mockResponses['/backend/activity/1/motion-without-words/'] = { 
-  url: 
-    '/backend/activity/1/motion-without-words/', 
-  title: 
-    'Motion Without Words',
-  pages: [
-    '/backend/activity/1/page/1', 
-    '/backend/activity/1/page/2', 
-    '/backend/activity/1/page/3',
-    '/backend/activity/1/page/4'
-  ],
-  pagesIndexUrl: '/backend/activity/1/pages/'
-};
-  
-Smartgraphs.mockResponses['/backend/activity/2/second-activity/'] = { 
-  url: 
-    '/backend/activity/2/second-activity/', 
-  title: 
-    'Example of Second Activity',
-  pages: [
-    '/backend/activity/2/page/1/'
-  ]
-};
+// Smartgraphs.mockResponses['/backend/activity/1/motion-without-words/'] = { 
+//   url: 
+//     '/backend/activity/1/motion-without-words/', 
+//   title: 
+//     'Motion Without Words',
+//   pages: [
+//     '/backend/activity/1/page/1', 
+//     '/backend/activity/1/page/2', 
+//     '/backend/activity/1/page/3',
+//     '/backend/activity/1/page/4'
+//   ],
+//   pagesIndexUrl: '/backend/activity/1/pages/'
+// };
+//   
+// Smartgraphs.mockResponses['/backend/activity/2/second-activity/'] = { 
+//   url: 
+//     '/backend/activity/2/second-activity/', 
+//   title: 
+//     'Example of Second Activity',
+//   pages: [
+//     '/backend/activity/2/page/1/'
+//   ]
+// };
