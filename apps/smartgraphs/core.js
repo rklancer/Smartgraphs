b449c23b463b422d968c9134a1554fbc7841348e
Richard Klancer <rpk@pobox.com>
2010-08-13 12:18:32 -0400

    Update mockResponses with no-trailing-slash style

--- apps/smartgraphs/core.js
+++ apps/smartgraphs/core.js
@@ -26,8 +26,8 @@ Smartgraphs = SC.Application.create(
   },
   
   // DEBUG SETTINGS
-  trace: NO,                  // whether to trace firstResponder changes and app actions
+  trace: NO,                   // whether to trace firstResponder changes and app actions
   useMockResponses: YES,       // whether the data source should use mock responses or real XHR        
-  logDataSource: YES           // whether the data source should log   
+  logDataSource: NO            // whether the data source should log   
   
 }) ;
