a6cb22d6c97c6cd17c46ffdd74f7c74638924533
Richard Klancer <rpk@pobox.com>
2010-08-25 00:40:48 -0400

    Bump RaphaelViews

--- apps/smartgraphs/core.js
+++ apps/smartgraphs/core.js
@@ -26,8 +26,8 @@ Smartgraphs = SC.Application.create(
   },
   
   // DEBUG SETTINGS
-  trace: YES,                   // whether to trace firstResponder changes and app actions
-  useMockResponses: NO,       // whether the data source should use mock responses or real XHR        
-  logDataSource: YES            // whether the data source should log   
+  trace: NO,                   // whether to trace firstResponder changes and app actions
+  useMockResponses: NO,        // whether the data source should use mock responses or real XHR        
+  logDataSource: NO            // whether the data source should log   
   
 }) ;
