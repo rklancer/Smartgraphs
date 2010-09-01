b5ea6e31a89dc75fcea50552ad96d8b65063fd8e
Richard Klancer <rpk@pobox.com>
2010-09-01 12:48:45 -0400

    Added afterPropertyChange test helper to debug folder

---
+++ apps/smartgraphs/debug/after_property_change.js
@@ -0,0 +1,104 @@
+// ==========================================================================
+// Project:   afterPropertyChange
+// Copyright: ©2010 Concord Consortium
+// @author:   Richard Klancer <rpk@pobox.com>
+// ==========================================================================
+/*globals ok equals stop start pushStop popStart */
+
+/*
+  afterPropertyChange:
+
+  Use this global function to test asynchronous behavior in Sproutcore by waiting to execute tests until  
+  desired Sproutcore property reaches a particular value (optionally, simply wait until the value changes.)
+
+  Use within a Qunit test() function like so:
+  
+  test('after myObject.isLoaded becomes YES, myApp.myData is defined', function () {
+    myObject.load();        // kick off asynchronous load
+
+    afterPropertyChange(myObject, 'isLoaded', YES, function () {
+      // this assertion is not tested until myObject.isLoaded becomes YES
+      ok(myObject.get('data'), 'myObject.data exists');
+    }
+  });
+  
+  Optionally, you can skip the third ('value') parameter and run the test callback whenver target.property changes
+  (or is notified):
+  
+  test('after myObject.load is called, its state should transition from LOADING to LOADED', function () {
+    myObject.load();
+    equals(myObject.get('state'), 'LOADING', "myObject.state was 'LOADING' immediately after myObject.load()");
+    
+    afterPropertyChange(myObject, 'state', function () {       // note the lack of the 'value' parameter
+      // this assertion is tested as soon as isLoaded is notified, whether via set() or notifyPropertyChange()
+      equals(myObject.get('state', 'LOADED', 'when myObject.state changed, it changed to LOADED');
+    }
+  });
+  
+  You can use this latter pattern to verify 'state transitions' as per the LOADING->LOADED transition above. If
+  the asynchronous callback changed myObject.state to anything but LOADED, the test would fail.
+  
+  NOTE: If you need to use stop() or start() to run asynchronous tests *within* the afterPropertyChange test 
+  callback itself, replace these with pushStop() popStart() respectively. afterPropertyChange uses stop() and
+  start() internally and Qunit does not like 
+  
+  NOTE: afterPropertyChange() always has to be called from within a test()
+  
+*/
+
+(function (undefined) {
+
+  var nStops = 0;
+
+  window.pushStop = function (t) {  
+    if (nStops === 0) stop(t);
+    nStops++;
+  };
+
+  window.popStart = function () {
+    --nStops;
+    if (nStops === 0) start();
+    if (nStops < 0) throw 'afterPropertyChange: popped too many starts';
+  };
+
+  window.afterPropertyChange = function (target, property, value, testFn) {
+    if (target && target.addObserver) {
+      // give a healthy 10s timeout to discourage anyone from depending on a timeout to signal failure
+      pushStop(10000);
+    }
+    else {
+      ok(false, 'afterPropertyChange: target is empty or does not have addObserver method.');
+      throw 'afterPropertyChange: target is empty or does not have addObserver method';
+    }
+  
+    // Allow alternate usage where the test proceeds as soon as the target.property changes
+    // For the sake of the readability of test cases, testFn should always be the last argument.
+    
+    if (testFn === undefined) {
+      testFn = value;
+      value = undefined;
+    }
+    if (!testFn) ok(false, 'afterPropertyChange: testFn is undefined.');
+  
+    function observer() {
+      // if a value is specified, do not proceed until the property attains that value
+      if (value !== undefined && target.get(property) !== value) return;
+    
+      target.removeObserver(property, observer);
+      try {
+        testFn();
+      }
+      catch (e) {
+        CoreTest.plan.error('Error during afterPropertyChange! See console log for details:', e);
+        console.error(e);
+        popStart();
+        return;
+      }
+      popStart();
+    }
+    target.addObserver(property, observer);
+  
+    // If a value is specified, check immediately to see if the property already has the specified value
+    if (value !== undefined) observer();
+  };
+}());
