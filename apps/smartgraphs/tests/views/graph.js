ff8d440bdd9816ae411d5350c16f6e61ecb4478c
Richard Klancer <rpk@pobox.com>
2010-09-02 11:22:36 -0400

    Start of unit test for GraphView

--- apps/smartgraphs/tests/views/graph.js
+++ apps/smartgraphs/tests/views/graph.js
@@ -2,14 +2,61 @@
 // Project:   Smartgraphs.GraphView Unit Test
 // Copyright: ©2010 My Company, Inc.
 // ==========================================================================
-/*globals Smartgraphs module test ok equals same stop start */
+/*globals Smartgraphs module test ok equals same stop start afterPropertyChange */
 
-module("Smartgraphs.GraphView");
+var pane;
+var view;
+var oldStore;
 
-// TODO: Replace with real unit test for Smartgraphs.GraphView
-test("test description", function() {
-  var expected = "test";
-  var result   = "test";
-  equals(result, expected, "test should equal test");
+module("Smartgraphs.GraphView", {
+  setup: function() {
+    Smartgraphs.Graph.FIXTURES = [
+      { url: 
+          '/backend/activity/1/graph/1/prediction-away',
+        name:
+          'Prediction-Away',
+        description: 
+          'Prediction graph of movement away',
+        axes:
+          '/backend/axes/1/5m-15s',
+        initialSeries: 
+          []
+      }
+    ];
+    
+    oldStore = Smartgraphs.store;
+    Smartgraphs.set('store', SC.Store.create().from(SC.Record.fixtures));
+    
+    Smartgraphs.firstGraphController.openGraph('/backend/activity/1/graph/1/prediction-away');
+    
+    SC.RunLoop.begin();
+    pane = SC.MainPane.create({
+      childViews: [
+        Smartgraphs.GraphView.design({
+          graphBinding: 'Smartgraphs.firstGraphController',
+          viewName: 'testGraphView'
+        })]
+    });
+    pane.append(); // make sure there is a layer...
+    SC.RunLoop.end();
+    
+    view = pane.childViews[0];
+  }, 
+  
+  teardown: function() {
+    pane.remove();
+    pane = view = null ;
+    Smartgraphs.set('store', oldStore);
+  }
 });
 
+test('show the view', function () {
+  
+  setTimeout(function () {
+    view.set('readyToTest', YES);
+  }, 5000);
+  
+  afterPropertyChange(view, 'readyToTest', YES, function () {
+    ok(true, 'you saw the graph, right?');
+  });
+});
