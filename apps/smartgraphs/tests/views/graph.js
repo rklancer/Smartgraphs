0fa3d133b99f5c17ce4e7326d9d793425b9da4a3
Richard Klancer <rpk@pobox.com>
2010-09-02 17:05:18 -0400

    Test that GraphView correctly adds and removes series views

--- apps/smartgraphs/tests/views/graph.js
+++ apps/smartgraphs/tests/views/graph.js
@@ -2,32 +2,57 @@
 // Project:   Smartgraphs.GraphView Unit Test
 // Copyright: ©2010 My Company, Inc.
 // ==========================================================================
-/*globals Smartgraphs module test ok equals same stop start afterPropertyChange */
+/*globals Smartgraphs RaphaelViews module test ok equals same stop start afterPropertyChange */
 
 var pane;
 var view;
+var canvasView;
 var oldStore;
 
+function setupFixtures() {
+  Smartgraphs.Graph.oldFixtures = Smartgraphs.Graph.FIXTURES;
+  
+  Smartgraphs.Graph.FIXTURES = [
+    { url: 'test',
+      axes: 'test-axes',
+      initialSeries: []
+    }
+  ];
+  
+  Smartgraphs.Axes.oldFixtures = Smartgraphs.Axes.FIXTURES;
+  Smartgraphs.Axes.FIXTURES = [
+    { url: 'test-axes',
+
+      xMin: 0,
+      xMax: 10,
+      xSteps: 15,
+      xLabel: 'xLabel (long)',
+      xLabelAbbreviated: 'xLabel (abbrev)',
+
+      yMin: 0,
+      yMax: 10,
+      ySteps: 25,
+      yLabel: 'yLabel (long)',
+      yLabelAbbreviated: 'yLabel (abbrev)'
+    }
+  ];
+  
+  oldStore = Smartgraphs.store;
+  Smartgraphs.set('store', SC.Store.create().from(SC.Record.fixtures));
+}
+
+function restoreFixtures() {
+  Smartgraphs.Graph.FIXTURES = Smartgraphs.Graph.oldFixtures;
+  Smartgraphs.Axes.FIXTURES = Smartgraphs.Axes.oldFixtures;
+  Smartgraphs.set('store', oldStore);
+}
+
+ 
 module("Smartgraphs.GraphView", {
   setup: function() {
-    Smartgraphs.Graph.FIXTURES = [
-      { url: 
-          '/backend/activity/1/graph/1/prediction-away',
-        name:
-          'Prediction-Away',
-        description: 
-          'Prediction graph of movement away',
-        axes:
-          '/backend/axes/1/5m-15s',
-        initialSeries: 
-          []
-      }
-    ];
+    setupFixtures();
     
-    oldStore = Smartgraphs.store;
-    Smartgraphs.set('store', SC.Store.create().from(SC.Record.fixtures));
-    
-    Smartgraphs.firstGraphController.openGraph('/backend/activity/1/graph/1/prediction-away');
+    Smartgraphs.firstGraphController.openGraph('test');
     
     SC.RunLoop.begin();
     pane = SC.MainPane.create({
@@ -37,26 +62,90 @@ module("Smartgraphs.GraphView", {
           viewName: 'testGraphView'
         })]
     });
-    pane.append(); // make sure there is a layer...
+    pane.append();
     SC.RunLoop.end();
     
-    view = pane.childViews[0];
+    view = pane.get('childViews').objectAt(0);
+    canvasView = view.get('graphCanvasView');
   }, 
   
-  teardown: function() {
+  teardown: function () {
     pane.remove();
-    pane = view = null ;
-    Smartgraphs.set('store', oldStore);
+    pane = view = null;
+    restoreFixtures();
   }
 });
 
-test('show the view', function () {
-  
-  setTimeout(function () {
-    view.set('readyToTest', YES);
-  }, 5000);
-  
-  afterPropertyChange(view, 'readyToTest', YES, function () {
-    ok(true, 'you saw the graph, right?');
-  });
+
+// test('show the view', function () {
+//   setTimeout(function () {
+//     view.set('readyToTest', YES);
+//   }, 5000);
+//   
+//   afterPropertyChange(view, 'readyToTest', YES, function () {
+//     ok(true, 'you saw the graph, right?');
+//   });
+// });
+
+
+test('GraphView should contain a child view that is RaphaelCanvasView', function () {
+  ok(SC.kindOf(canvasView, RaphaelViews.RaphaelCanvasView), 'GraphView.graphCanvasView is a RaphaelCanvasView');
 });
+
+
+test('adding and removing DataSeries from the graph controller should result in a calls to appendChild and removeChild', function () {
+  var seriesList = Smartgraphs.firstGraphController.get('seriesList');
+  var series1 = Smartgraphs.store.createRecord(Smartgraphs.DataSeries, { url: 'series1' });
+  var series1View = null;
+  var series2 = Smartgraphs.store.createRecord(Smartgraphs.DataSeries, { url: 'series2' });
+  var series2View = null;
+  var viewAppended = null;
+  var viewRemoved = null;
+  
+  canvasView.oldAppendChild = canvasView.appendChild;
+  canvasView.appendChild = function (v) {
+    viewAppended = v;
+    this.oldAppendChild(v);
+  };
+
+  canvasView.oldRemoveChild = canvasView.removeChild;
+  canvasView.removeChild = function (v) {
+    viewRemoved = v;
+    this.oldRemoveChild(v);
+  };
+  
+  seriesList.pushObject(series1);
+  ok(SC.kindOf(viewAppended, RaphaelViews.RaphaelCollectionView), 'a RaphaelCollectionView was appended after series1 was pushed onto seriesList');
+
+  series1View = viewAppended;
+
+  seriesList.pushObject(series2);
+  ok(SC.kindOf(viewAppended, RaphaelViews.RaphaelCollectionView), 'a RaphaelCollectionView was appended after series2 was pushed onto seriesList');
+  
+  series2View = viewAppended;
+
+  ok(viewRemoved === null, 'No views were removed when series were added');
+  
+  seriesList.removeObject(series1);
+  ok(viewRemoved === series1View, 'The view for series1 was removed when series1 was removed from the seriesList');
+  
+  seriesList.removeObject(series2);
+  ok(viewRemoved === series2View, 'The view for series2 was removed when series2 was removed from the seriesList');  
+});
+
+
+// test that coordinatesForPoint and pointForCoordinates are inverse
+// test that above 2 are linear, check that they return sane values for extreme end of ranges
+
+// what about changing axes?
+
+// test that axes get rendered
+// add a point to a series and test it gets rendered (various permutations here)
+// test that labels get rendered only when visible
+
+// test that adding a series that already has data renders it correctly.
+
+
+
+
+
