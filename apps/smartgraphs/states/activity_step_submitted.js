746ffc6a8201532e00a0bcdd34d8fb663a4ee2e4
Richard Klancer <rpk@pobox.com>
2010-09-11 18:57:03 -0400

    Note the error if we stop && isLastStep == NO

--- apps/smartgraphs/states/activity_step_submitted.js
+++ apps/smartgraphs/states/activity_step_submitted.js
@@ -26,6 +26,9 @@ Smartgraphs.ACTIVITY_STEP_SUBMITTED = SC.Responder.create(
     var newStep = Smartgraphs.activityStepController.get('content');
     // if we didn't change steps after submission completed, then there must be no more steps for this page.
     if (newStep === oldStep) {
+      if (!oldStep.get('isLastStep')) {
+        console.error('activityStepController.isLastStep is NO but handleSubmission() did not move to a new step');
+      }
       Smartgraphs.makeFirstResponder(Smartgraphs.ACTIVITY_PAGE_DONE);
     }
   },
