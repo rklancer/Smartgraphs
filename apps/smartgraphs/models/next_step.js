0a3ecbb0edf105c77543bed56b745a77da40e329
Richard Klancer <rpk@pobox.com>
2010-09-11 13:47:34 -0400

    new models: 'NextStep' and 'TriggeredCommands'

---
+++ apps/smartgraphs/models/next_step.js
@@ -0,0 +1,39 @@
+// ==========================================================================
+// Project:   Smartgraphs.NextStep
+// Copyright: ©2010 Concord Consortium
+// @author    Richard Klancer <rpk@pobox.com>
+// ==========================================================================
+/*globals Smartgraphs */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Smartgraphs.NextStep = SC.Record.extend(
+/** @scope Smartgraphs.NextStep.prototype */ {
+
+  /**
+    The parent step whose response we are evaluating
+  */
+  parentStep: SC.Record.toOne('Smartgraph.ActivityStep'),
+  
+  /**
+    The order in which this criterion will be evaluated
+  */
+  index: SC.Record.attr(Number),
+  
+  /**
+    JSON expression tree that converts the parent step's response Inspector value into YES or NO
+  */
+  responseCriterion: SC.Record.attr(Object),
+  
+  /**
+    The step to jump to if the responseCriterion evaluates to YES
+    (if the step evaluates to NO, the next criterion-step pair will be tested)
+  */
+  step: SC.Record.toOne('Smartgraphs.ActivityStep')
+
+}) ;
---
+++ apps/smartgraphs/tests/models/triggered_commands.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Smartgraphs.TriggeredCommands Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Smartgraphs module test ok equals same stop start */
+
+module("Smartgraphs.TriggeredCommands");
+
+// TODO: Replace with real unit test for TriggeredCommands
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
