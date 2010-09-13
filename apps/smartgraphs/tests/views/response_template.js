7fb5c89d99b5c34bcc57b55484ec534924d8d7fb
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-09-13 14:58:47 -0700

    Fixed unit test for Smartgraphs.ResponseTemplate

--- apps/smartgraphs/tests/views/response_template.js
+++ apps/smartgraphs/tests/views/response_template.js
@@ -47,12 +47,14 @@ module("Smartgraphs.ResponseTemplate", {
 
 });
 
-test("multiple choice question should contain choice radio buttons ", function () {
+test("multiple choice question should contain choice radio buttons ",
+function() {
 
     var responseTemplates = Smartgraphs.store.find(Smartgraphs.ResponseTemplate);
     var responseTemplate = responseTemplates.objectAt(0);
-    
-    equals(responseTemplate.get('attributes'), Smartgraphs.ResponseTemplate.FIXTURES[0], "responseTemplate.get('attributes') should equal Smartgraphs.ResponseTemplate.FIXTURES[0]");
+
+    equals(responseTemplate.get('attributes'), Smartgraphs.ResponseTemplate.FIXTURES[0], 
+		"responseTemplate.get('attributes') should equal Smartgraphs.ResponseTemplate.FIXTURES[0]");
 
     Smartgraphs.responseTemplateController.set('content', responseTemplate);
 
@@ -63,51 +65,35 @@ test("multiple choice question should contain choice radio buttons ", function (
         editingShouldBeEnabledBinding: 'Smartgraphs.responseTemplateController.editingShouldBeEnabled'
     });
 
-    // this is not what you want; the view will be created implicitly when pane.append() is called:
-    // var responseTemplateView = responseTemplateDesign.create({});
-    
     SC.RunLoop.begin();
     pane = SC.MainPane.create({
         // childViews arrays should contain classes (i.e., the result of [viewClass].design() or [viewClass].extend())
         // NOT view instances; if you do *create* a view, you need to use [parentView].appendChild() to give the 
         // created view a parent.
-        childViews: [responseTemplateDesign]      // changed to responseTemplateDesign from responseTemplateView
+        childViews: [responseTemplateDesign]
     });
     pane.append();
-    console.log("pane:", pane);
+    // console.log("pane:", pane);
     SC.RunLoop.end();
 
     // after pane.append(), you can get the view this way:
     responseTemplateViewRendered = pane.get('childViews').objectAt(0);
-    
-    // console.log("responseTemplateViewRendered:", responseTemplateViewRendered.toString());
-    // console.log("responseTemplateViewRendered.get('childViews'):", responseTemplateViewRendered.get('childViews'));
-    // console.log("responseTemplateViewRendered.get('childViews').get('length'):", responseTemplateViewRendered.get('childViews').get('length'));
-    // console.log("responseTemplateViewRendered.get('childViews').objectAt(0):", responseTemplateViewRendered.get('childViews').objectAt(0));
-    // console.log("responseTemplateViewRendered.get('childViews').objectAt(0).get('childViews').objectAt(0):", 
-	
-		// instead of logging so much (which makes the test impossible to read), just check that the view is what you want:
+
     ok(SC.kindOf(responseTemplateViewRendered, Smartgraphs.ResponseTemplateView), "responseTemplateView should have been rendered");
     ok(responseTemplateViewRendered.get('childViews').objectAt(0), "responseTemplateView should have a childView");
 
-    //TODO: fix testing code below that was mostly copied from CC framework
     var multipleChoiceQuestionView = responseTemplateViewRendered.get('childViews').objectAt(0).get('childViews').objectAt(0);
-
-    // again, these console.logs interrupt the flow of the test; try just using asserting that the things you're 
-    // logging "equals()" the things you expect them to be. (If they're not, equals() will show their actual value.)
 
     // console.log("multipleChoiceQuestionView:", multipleChoiceQuestionView);
     // console.log("multipleChoiceQuestionView.get('childViews'):", multipleChoiceQuestionView.get('childViews'));
     // console.log("multipleChoiceQuestionView.get('childViews').objectAt(1):", multipleChoiceQuestionView.get('childViews').objectAt(1));
-    // console.log("Don't know why responseTemplateViewRendered.$('.question-input') is not the rendered inputView div but is:", responseTemplateViewRendered.$('.question-input'));
-
-    // '$input' below is a jQuery object wrapping the selected DOM elements (those with class 'question-input');
+    // console.log("responseTemplateViewRendered.$('.question-input'):", responseTemplateViewRendered.$('.question-input'));
+    //
+	// '$input' below is a jQuery object wrapping the selected DOM elements (those with class 'question-input');
     // I like the convention of prefixing jQuery objects with '$'
     var $input = responseTemplateViewRendered.$('.question-input');
-
     // to get the DOM element selected by $() (which is more amenable to console.log than is the jQuery object)
-    // use array notation:
-    
+    // use array notation:    
     console.log("$input[0]: ", $input[0]);
 
     // equals will tell you the actual value
@@ -115,9 +101,9 @@ test("multiple choice question should contain choice radio buttons ", function (
 
     var firstChoice = $input.find('.sc-button-label').first().html().strip();
     console.log("firstChoice: ", firstChoice);
-    equals(firstChoice, "a", "first choice should be 'a'");
+    equals(firstChoice, "a2", "first choice should be 'a2'");
 
     var lastChoice = $input.find('.sc-button-label').last().html().strip();
     console.log("lastChoice: ", lastChoice);
-    equals(lastChoice, "c", "last choice should be 'c'");
+    equals(lastChoice, "c2", "last choice should be 'c2'");
 });
