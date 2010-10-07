025961a888646bc29fb93abd40731948043afa1f
Richard Klancer <rpk@pobox.com>
2010-10-07 12:10:22 -0400

    Highlighted point for Maria's run

--- apps/smartgraphs/fixtures/highlighted_point.js
+++ apps/smartgraphs/fixtures/highlighted_point.js
@@ -9,11 +9,19 @@ sc_require('models/highlighted_point');
 Smartgraphs.HighlightedPoint.FIXTURES = [
 
   // need to have some fixtures or fixturesDataSource's updateRecords() thinks it can't handle the record.
+  // http://github.com/sproutcore/sproutcore/issues/#issue/36
   { url: '/backend/activity/1/annotations/walking-first-point',
     name: 'walking-first-point',
     isExample: YES,
     session: null,
     point: 1
+  },
+
+  { url: '/backend/activity/2/annotations/where-maria-stopped',
+    name: 'where-maria-stopped',
+    isExample: YES,
+    session: null,
+    point: 403
   }
 
 ];
