596c1c7e8cb85bed06074f90c4b6880ecf2434e9
Dr. Baba Kofi Weusijana <kweusijana@concord.org>
2010-10-13 13:26:02 -0700

    Unit tests for LineToAxis and HighlightedSegment

--- apps/smartgraphs/tests/views/annotation_views.js
+++ apps/smartgraphs/tests/views/annotation_views.js
@@ -119,42 +119,97 @@ module("Smartgraphs Annotation View instantiation", {
 });
 
 
-test('HighlightedPoint location should track the point it highlights', function () {  
-  var highlight = Smartgraphs.sessionController.createAnnotation(Smartgraphs.HighlightedPoint, 'test-highlight');
+test('HighlightedPoint location should track the point it highlights', function () {
+  var highlightedPoint = Smartgraphs.sessionController.createAnnotation(Smartgraphs.HighlightedPoint, 'test-highlightedPoint');
   var point = addPoint(dataset, 1, 5);
-  highlight.set('point', point);
-  Smartgraphs.firstGraphController.addAnnotation(highlight);
+  highlightedPoint.set('point', point);
+  Smartgraphs.firstGraphController.addAnnotation(highlightedPoint);
 
-  var highlightView = childViews.objectAt(childViews.get('length') - 1);  
-  ok(SC.kindOf(highlightView, Smartgraphs.HighlightedPointView), 'a highlightedPointView should have been added to the graph canvas');
+  var highlightedPointView = childViews.objectAt(childViews.get('length') - 1);
+  ok(SC.kindOf(highlightedPointView, Smartgraphs.HighlightedPointView), 'a highlightedPointView should have been added to the graph canvas');
 
   // let the view render
   SC.RunLoop.begin();
   SC.RunLoop.end();
-  
-  var r = highlightView.get('raphaelObject');
-  var attr = r.attr();
+
+  var raphaelObject = highlightedPointView.get('raphaelObject');
+  var attr = raphaelObject.attr();
   var coords = graphView.coordinatesForPoint(1, 5);
-  
-  equals(attr.cx, coords.x, "highlight's center-x should be x-coordinate of point (1,5)");
-  equals(attr.cy, coords.y, "highlight's center-y should be y-coordinate of point (1,5)");
-  
+
+  equals(attr.cx, coords.x, "highlightedPoint's center-x should be x-coordinate of point (1,5)");
+  equals(attr.cy, coords.y, "highlightedPoint's center-y should be y-coordinate of point (1,5)");
+
   SC.RunLoop.begin();
   point.set('x', 2);
   SC.RunLoop.end();
-  
-  attr = r.attr();
+
+  attr = raphaelObject.attr();
   coords = graphView.coordinatesForPoint(2, 5);
-  equals(attr.cx, coords.x, "after update of x value highlight's center-x should be x-coordinate of point (2,5)");
-  equals(attr.cy, coords.y, "after update of x value, highlighted center-y should be y-coordinate of point (2,5)");
-  
+  equals(attr.cx, coords.x, "after update of x value highlightedPoint's center-x should be x-coordinate of point (2,5)");
+  equals(attr.cy, coords.y, "after update of x value, highlightedPoint center-y should be y-coordinate of point (2,5)");
+
   SC.RunLoop.begin();
   point.set('y', 6);
   SC.RunLoop.end();
 
-  attr = r.attr();
+  attr = raphaelObject.attr();
   coords = graphView.coordinatesForPoint(2, 6);
-  equals(attr.cx, coords.x, "after update of x value highlight's center-x should be x-coordinate of point (2,6)");
-  equals(attr.cy, coords.y, "after update of x value, highlighted center-y should be y-coordinate of point (2,6)");
-  
+  equals(attr.cx, coords.x, "after update of x value highlightedPoint's center-x should be x-coordinate of point (2,6)");
+  equals(attr.cy, coords.y, "after update of x value, highlightedPoint center-y should be y-coordinate of point (2,6)");
+
+});
+
+test('LineToAxis location should have the expected path with the starting point it highlights', function () {
+  var lineToAxis = Smartgraphs.sessionController.createAnnotation(Smartgraphs.LineToAxis, 'test-lineToAxis');
+  var point = addPoint(dataset, 1, 5);
+  lineToAxis.set('point', point);
+  Smartgraphs.firstGraphController.addAnnotation(lineToAxis);
+
+  var lineToAxisView = childViews.objectAt(childViews.get('length') - 1);
+  ok(SC.kindOf(lineToAxisView, Smartgraphs.LineToAxisView),
+    'a lineToAxisView should have been added to the graph canvas');
+
+  // let the view render
+  SC.RunLoop.begin();
+  SC.RunLoop.end();
+
+  var raphaelObject = lineToAxisView.get('raphaelObject');
+  var attr = raphaelObject.attr();
+  var startingCoords = graphView.coordinatesForPoint(point.get('x'), point.get('y'));
+  var endingCoords = graphView.coordinatesForPoint(0, point.get('y'));
+
+  ok(!attr.shouldHideLinePath, "lineToAxis's shouldHideLinePath should be NO or undefined");
+  equals(attr.path[0][1], startingCoords.x, "lineToAxis's starting-x should be " + startingCoords.x.toString() +
+    " for point:" + point.toString());
+  equals(attr.path[0][2], startingCoords.y, "lineToAxis's starting-y should be " + startingCoords.y.toString());
+  equals(attr.path[1][1], endingCoords.x, "lineToAxis's ending-x should be " + endingCoords.x.toString());
+  equals(attr.path[1][2], endingCoords.y, "lineToAxis's ending-y should be " + endingCoords.y.toString());
+});
+
+test('HighlightedSegment location should have the expected path with the points it highlights', function () {
+  var highlightedSegment = Smartgraphs.sessionController.createAnnotation(Smartgraphs.HighlightedSegment, 'test-highlightedSegment');
+  var pointA = addPoint(dataset, 1, 5);
+  var pointB = addPoint(dataset, 2, 6);
+  highlightedSegment.set('points', [pointA, pointB]);
+  Smartgraphs.firstGraphController.addAnnotation(highlightedSegment);
+
+  var highlightedSegmentView = childViews.objectAt(childViews.get('length') - 1);
+  ok(SC.kindOf(highlightedSegmentView, Smartgraphs.HighlightedSegmentView),
+    'a highlightedSegmentView should have been added to the graph canvas');
+
+  // let the view render
+  SC.RunLoop.begin();
+  SC.RunLoop.end();
+
+  var raphaelObject = highlightedSegmentView.get('raphaelObject');
+  var attr = raphaelObject.attr();
+  var startingCoords = graphView.coordinatesForPoint(pointA.get('x'), pointA.get('y'));
+  var endingCoords = graphView.coordinatesForPoint(pointB.get('x'), pointB.get('y'));
+
+  equals(attr.path[0][1], startingCoords.x, "highlightedSegment's starting-x should be " + startingCoords.x.toString() +
+    " for pointA:" + pointA.toString());
+  equals(attr.path[0][2], startingCoords.y, "highlightedSegment's starting-y should be " + startingCoords.y.toString());
+  equals(attr.path[1][1], endingCoords.x, "highlightedSegment's ending-x should be " + endingCoords.x.toString() +
+    " for pointB:" + pointB.toString());
+  equals(attr.path[1][2], endingCoords.y, "highlightedSegment's ending-y should be " + endingCoords.y.toString());
 });
