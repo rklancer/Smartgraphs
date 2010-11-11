38b6ca286d3ca3425754107eb3a08a292e59677a
Richard Klancer <rpk@pobox.com>
2010-11-11 18:55:04 -0500

    Document the Session model

--- apps/smartgraphs/models/session.js
+++ apps/smartgraphs/models/session.js
@@ -7,14 +7,27 @@
 
 /** @class
 
-  (Document your Model here)
+  A Session represents a single run of an activity.
+  
+  This allows an activity to be run multiple times within a single browser 'session'. User-created objects that are 
+  referenced by name (such as Datasets and Annotations) can be scoped to the current Session, so that references to
+  a dataset (for example) with a given name call up data created by the current user within the current session of the
+  activity.
 
+  Sessions are created in the LOADING_ACTIVITY state (which is revisited whenever an Activity is started from the 
+  beginning.)
+  
   @extends SC.Record
   @version 0.1
 */
 Smartgraphs.Session = SC.Record.extend(
 /** @scope Smartgraphs.Session.prototype */ {
 
+  /**
+    The User who is logged in during the Session.
+    
+    @property {Smartgraphs.User}
+  */
   user: SC.Record.toOne('Smartgraphs.User', { inverse: 'session', isMaster: YES }),
   
   steps: SC.Record.toMany('Smartgraphs.  SessionStep', { inverse: 'session' })
