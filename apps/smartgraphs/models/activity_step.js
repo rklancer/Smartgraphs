9838e73488d9893267995ce169fc994a7421f329
Richard Klancer <rpk@pobox.com>
2010-11-12 18:28:02 -0500

    Extensively document ActivityStep model.

--- apps/smartgraphs/models/activity_step.js
+++ apps/smartgraphs/models/activity_step.js
@@ -7,7 +7,68 @@
 
 /** @class
 
-  (Document your Model here)
+  An ActivityStep record specifies what is shown, and what interactions may occur, during a given step on an
+  ActivityPage. It also specifies what step, if any, should be visited in response to the interaction that occurred
+  during the step. As such ActivityStep is the most complex model in the Smartgraphs codebase.
+  
+  Bear in mind that ActivitySteps branch. The ActivitySteps in an ActivityPage are not necessarily visited in order;
+  nor are they necessarily all visited at all.
+  
+  Furthermore, the currently loaded ActivityStep is considered to have substates. When a step loads, the system enters
+  the ACTIVITY_STEP state. ACTIVITY_STEP (or one of its substates, i.e., a state like SENSOR_RECORDING that has
+  ACTIVITY_STEP in its nextResponder chain) remains the current state while the user interacts with the Smartgraph
+  application. When the user is done interacting with that step -- perhaps they have chosen an answer, or entered data
+  to their satisfaction -- they can click a button to "submit" the step for consideration by the system. 
+  
+  Submitting is only allowed when the step is in a "submissible" state; see below for details of how an ActivityStep
+  specifies how to configure the system to observe its state and turn submissibility on or off based on that state.
+  Moreover submission can be performed programmatically, rather than in response to a button click.
+  
+  Steps specify how to inspect the system state and choose the next step to load once the step is submitted.
+  
+  h3. What is specified in an ActivityStep record
+  
+   - the configuration of the panes on the right (split or single)
+
+   - what graph, table, or image to display in each pane, and what datasets or annotations they should show.
+
+   - any commands to be executed when the step starts, before user interaction is allowed
+
+   - the "student's response" form to be included in the pane on the left
+
+   - any text (such as question prompt or hints) to be displayed before and after the response form
+   
+   - whether this automatically move to the submitted state when it loads (this could be used to create a step whose only purpose is to execute some commands)
+   
+   - a "submissibility" Inspector which continuously watches the system state (such as the currently-entered value in the response form, or the contents of the student's current sketch) and reduces it to a summary value (such as the value entered into that form, or the total path length of the student's sketch) whenever that state changes
+      
+   - an expression that translates that inspector's value to YES or NO whenever it changes, and that is used to enable submission only when its value is YES
+
+   - a set of commands to be executed immediately after the user submits the step
+  
+   - a response Inspector which inspects the system state once when student submits the step and reduces it to a summary value
+
+   - a set of expressions that translate the response inspector's value into YES or NO values in order to determine which step to load next
+     
+   - whether this is a 'terminal' step; ie., whether failure to branch away from this step should be taken to mean that we have finished the ActivityPage itself
+     
+   - whether the UI should or should not show a button that submits this step;
+   
+   - the title of that button
+   
+   - whether the 'Next Page' button should be enabled, and should both submit the step and move on to the next page, when this step is submissible. (This is a shortcut so that, on the last step in a page, users don't have to click Submit and then immediately click Next Page)
+     
+  h3. Serializing an ActivityStep
+  
+  When editing the serialized form of an ActivityStep, bear in mind that value of many of the properties are expected
+  to be Javascript hashes. Where possible the expected content of these hashes is described.
+  
+  Generally, those hashes reference Activity objects by name, rather than by url (or id), but exceptions are noted
+  below.
+  
+  Where properties of the ActivityPage are explicitly documented to be record types rather than strings, then the
+  associated property must be specified in the serialized format via the url or id of the relevant object, rather than
+  by name.
 
   @extends SC.Record
   @version 0.1
@@ -15,151 +76,322 @@
 Smartgraphs.ActivityStep = SC.Record.extend(
 /** @scope Smartgraphs.ActivityStep.prototype */ {
 
+  /**
+    The primary key of an ActivityStep record is its url.
+    
+    The usual format is /<username>/<activityname>/page/nnn/step/nnn
+    
+    @property {String}
+  */
   url: SC.Record.attr(String),
   primaryKey: 'url',
   
   /** 
     The ActivityPage this ActivityStep is a part of.
+    
+    @property {Smartgraphs.ActivityPage}
   */
   activityPage: SC.Record.toOne('Smartgraphs.ActivityPage', { inverse: 'steps', isMaster: YES }),
   
   /**
-    Whether to show a split pane or single pane (value is "split" or "single")
+    Whether to show a split or single pane on the right hand side of the screen. Acceptable values are "split" or 
+    "single".
+    
+    @property {String}
   */  
   paneConfig: SC.Record.toOne(String),
   
   /**
-    How to configure the panes, ex:
+    A hash that represents how to configure the panes being shown on the right hand side. If a split pane is showing,
+    expects two keys, "top" and "bottom"; if a single pane is showing, expects the single key "single".
     
-    "panes": {
-      "top": {
-        "type": "graph",
-        "name": "sensor-playing"
-      },
-      "bottom": {
-        "type": "table",
-        "graphName": "sensor-playing",
-        "datasetName": "sensor-play"
+    The object at those keys specifies what is to be shown in the associated pane. The value at key "type" specifies
+    whether a graph, table, or image is shown. The other keys in the object specify configuration, as shown below:
+    
+    {{{
+      {
+        "top": {
+          "type": "graph",
+          "name": "sensor-playing"
+        },
+        "bottom": {
+          "type": "table",
+          "graphName": "sensor-playing",
+          "datasetName": "sensor-play"
+        }
       }
-    }
+    }}}
+    
+    or 
+    
+    
+    {{{
+      {
+        "single": { 
+          "type": "image", 
+          "path": sc_static("resources/arrow.jpg")
+      }
+    }}}   
+    
+    @property {Object}
   */
   panes: SC.Record.attr(Object),
   
   /**
-    Text to display *before* the response template
+    HTML-formatted text to display above the response template. Text here should be wrapped within html elements such
+    as &lt;h1&gt; or &lt;p&gt; in order to be correctly styled.
+    
+    @property {String}
   */
   beforeText: SC.Record.attr(String),                                           
   
-  /** 
+  /**
     The responseTemplate. If null, none is shown. If set to a ResponseTemplate, it displays an 'input form' between
-    the beforeText and the afterText
+    the beforeText and the afterText.
+     
+    @property {Smartgraphs.ResponseTemplate}
   */
   responseTemplate: SC.Record.toOne('Smartgraphs.ResponseTemplate'),
   
   /**
-    Text to display *after* the response template
+    Text to display below the response template. Text here should be wrapped within html elements such as &lt;h1&gt; 
+    or &lt;p&gt; in order to be correctly styled.
+    
+    @property {String}    
   */
   afterText: SC.Record.attr(String),
 
   /** 
-    The list of commands (and their arguments) to be run when this ActivityStep is loaded.
+    A list of commands (and their arguments) to be run when this ActivityStep is loaded. Each item in the list should
+    be an object with an 'action' property and a 'literalArgs' property. The arguments in the literalArgs will be 
+    passed to the action. The actions available here are specified in the ACTIVITY_STEP, ACTIVITY, and READY states.
+   
+    {{{
+      [
+        {
+          "action": "startFreehandInput",
+          "literalArgs": {
+             "graphName": "prediction-away",
+             "annotationName": "prediction-away"
+          }
+        }
+      ]
+     }}}
+   
+     @property {Array}
   */
   startCommands: SC.Record.attr(Array),
   
   /**
-    Whether to 'submit' (aka finish) automatically as soon as the startCommands execute, or whether to wait for
-    the user to click 'submit'/'done' button
+    If YES, the activity step will automatically be submitted immediately after the startCommands execute. Otherwise,
+    the user must explicitly click the submit button, or submission must occur as a side effect of a command that
+    executes during the step.
+    
+    This is useful for pages that contain a single step that non-interactively displays information. By automatically
+    finishing the step, the "Next Page" becomes enabled, and the user can advance to the next page at will.
+    
+    @property {Boolean}
   */
   shouldFinishImmediately: SC.Record.attr(Boolean),
   
   /**
-    Whether to turn submissibility off at the beginning of the step and wait for submissibility to become true
-    before allowing the user to click submit/done.
-    (The alternative is that the user can click 'done' without taking any prior step, as we might want if the step is
+    Whether to turn submissibility off at the beginning of the step and wait for submissibility to become true before
+    allowing the user to submit the step.
+    
+     (The alternative is that the user can click 'done' without taking any prior step, as we might want if the step is
     purely informative or if the users' action is optional.)
+    
+    @property {Boolean}
   */
   shouldWaitForSubmissibleResponse: SC.Record.attr(Boolean),
   
   /**
-    a hash that contains:
-      the class name of the Inspector that checks system state for submissibility
-      a config hash to be passed to the inspector when created
+    A hash that specifies an Inspector class that will be instantiated to continually monitor the system state for
+    submissibility during this step, and a config hash that will be passed to the Inspector's config method to
+    configure the Inspector.
+
+    Example:
+
+    {{{
+      {
+        "type": "Smartgraphs.SketchLengthInspector",
+        "config": {
+          "annotationName": "prediction",
+          "check": "continuously"
+        }
+      }
+    }}}
+    
+    @property {Object}
   */
   submissibilityInspector: SC.Record.attr(Object),
 
   /** 
-    JSON expression tree to be used to convert the Inspector's output to a YES or NO answer.
+    A JSON expression tree that will be used to convert the submissibilityInspector's output to a YES or NO answer
+    whenever the submissibilityInspector's output changes. 
+    
+    When the output changes from YES to NO, submissibility is disabled. When it transitions from NO to YES,
+    submissibility is enabled.
+    
+    The expression format will be documented elsewhere; note that the sub-expression "value" will be substituted
+    by the value of the submissibilityInspector.
+    
+    {{{
+      {
+        "gt": ["value", 5]
+      }
+    }}}
+    
+    @property {Object}
   */
   submissibilityCriterion: SC.Record.attr(Object),
   
   /**
-    A list of (systemInspector, triggerCriterion, onCommands, offCommands) sets
-      * the systemInspector is registered to observe the system state while the step is waiting and produce a
-        value whenever the relevant state changes
-      * the triggerCriterion turns the value into a boolean
-      * when the boolean goes from NO to YES, the onCommands are run
-      * when the boolean goes from YES to NO, the offCommands are run
+    Currently unimplemented. Commands that can be triggered to execute when the student manipulates the system into a
+    certain state.
+    
+    The idea is that this would consist of a list of (systemInspector, triggerCriterion, onCommands, offCommands) sets
+     - The systemInspector is registered to observe the system state while the step is waiting and produce a value whenever the relevant state changes
+     - the triggerCriterion turns the value into a boolean
+     - when the boolean goes from NO to YES, the onCommands are run
+     - when the boolean goes from YES to NO, the offCommands are run
       
-      (Possible improvement: each systemInspector gets a *list* of (triggerCriterion, onCommand, offCommand) sets
-      
-      These can do things like run commands immediately when the student's graph acquires certain features
+    These triggered commands do things like run commands immediately when the student's input acquires certain
+    features.
+    
+    @property {Smartgraphs.TriggeredCommands[]}
   */
   triggeredCommands: SC.Record.toMany('Smartgraphs.TriggeredCommands'),  
   
-  /** 
-    The list of commands (and their arguments) to be run right after this ActivityStep is submitted.
+  /**
+    The list of commands (and their arguments) to be immediately after this ActivityStep is submitted, before the
+    response inspector is consulted and before branching to another step.
+    
+    The format of this list is the same as for the startCommands <i>(q.v.)</i>. However, these commands execute while 
+    the system is in the ACTIVITY_STEP_SUBMITTED state. Therefore only the commands defined in ACTIVITY_STEP_SUBMITTED
+    and its nextResponders (ACTIVITY, READY) are available in this context.
+    
+    @property {Array}
   */
   afterSubmissionCommands: SC.Record.attr(Array),
   
   /**
-    a hash that contains:
-      the class name of the Inspector that checks the user's submitted response, if any.
-      a config hash to be passed to the inspector when created
+    A hash that specifies an Inspector class that will be instantiated to inspect the state of the system after the
+    step is submitted, and that specifies a config hash that is passed to the config method of the Inspector class.
+    This is used to decide the system's response to the user's manipulation of system state (which may represent the
+    user's answer to a question or challenge posed by the ActivityStep) prior to submitting the ActivityStep.
+    
+    The Inspector's <code>inspect()</code> method will be called once, after the step is submitted; the value returned
+    by the inspect method will be used to determine the next ActivityStep to load, as per the specification in the 
+    responseBranches property of the activityStep.
+    
+    For example, this value of <code>responseInspector</code> will configure the system to consider the student's
+    response to be the selected point in dataset 'maria' on graph 'maria'.
+    {{{
+      {
+        "type": "Smartgraphs.SelectedPointInspector",
+        "config": {
+          "graphName": "maria",
+          "datasetName": "maria"
+        }
+      }
+    }}}
+    
+    @property {Object}
   */
   responseInspector: SC.Record.attr(Object),
   
   /**
-    An ordered list of criterion -> ActivityStep pairs
+    An ordered list of criterion -> ActivityStep pairs that is used to choose the next ActivityStep to load after
+    the student has submitted the step and the ResponseInspector has evaluated the system state.
     
     After response is submitted, each criterion is evaluated in order, with the return value of the responseInspector
-    as the value . The system jumps to the ActivityStep associated with the first criterion that evaluates to YES.
-    
+    as the value. The system jumps to the ActivityStep associated with the first criterion that evaluates to YES.
     Think of an if-else chain.
     
-    If no reactionCriterion evaluates to YES, the defaultBranch is jumped to, if it exists
+    If no reactionCriterion evaluates to YES, the ActivityStep specified by the <code>defaultBranch</code> property is
+    jumped to, if one is specified.
+    
+    For example, this value of <code>responseBranches</code> will cause the system to open the ActivityStep
+    "/shared/marias-run/page/5/step/6" if the 'xvalue' of the responseInspector value is equal to 4. (If it is not,
+    the system will attempt to load the step specified in the defaultBranch.)
+    {{{
+      [
+        {
+          "criterion": {
+            "equals": [
+              {
+                "xvalue": "value"
+              },
+              4
+            ]
+          },
+          "step": "/shared/marias-run/page/5/step/6"
+        }
+      ]
+    }}}
+
+    Note that steps are specified by url.
+
+    @property {Object[]}
   */
   responseBranches: SC.Record.attr(Array),
   
+  /**
+    The ActivityStep that will be loaded after the step is submitted, if no responseBranch is chosen.
+
+    @property {Smartgraphs.ActivityStep}
+  */
   defaultBranch: SC.Record.toOne('Smartgraphs.ActivityStep'),
   
   /** 
-    If, after the step is finished/submitted, we don't jump to any new step *AND* isFinalStep === NO for this step,
-    we know there was an error.
+    If this value is YES, this step is a 'terminal step', indicating that, if this step completes without branching
+    to a new ActivityStep, then the user has finished the ActivityPage.
+    
+    If, after the step is submitted, the system has not jumped to new step and isFinalStep === NO for the current,
+    there was an error in the activity definition.
+    
+    @property {Boolean}
   */
   isFinalStep: SC.Record.attr(Boolean),
   
   /**
-    if we are the last step, whether to automatically skip to the next page when this step finishes.
-    FIXME. Different from nextButtonShouldSubmit because (nominally) it leaves the submit button visible and the next
-    button hidden; unsure if we want to implement both this and nextButtonShouldSubmit
-  */
-  shouldAutoAdvancePage: SC.Record.attr(Boolean),
-  
-  /**
-     Whether the submit/done button needs to be hidden or not
-     Might be YES for steps that submit automatically when the student's responses acquires certain characteristics
+    If YES, the submit button should be hidden.
+    Might be YES for steps that submit automatically when the student's responses acquires certain characteristics
+    
+    @property {Boolean}
   */
   hideSubmitButton: SC.Record.attr(Boolean),
 
   /**
-    The title of the submit/done button
+    The title of the submit button, if it is displayed. 
+    
+    The button will be enabled or not according to whether step submission is currently enabled. (e.g., the
+    submissibilityInspector/submissiblityCriterion pair dynamically determine whether this button is enabled.)
+    
+    @property {String}
   */
   submitButtonTitle: SC.Record.attr(String),
   
   /**
-    Whether the 'next page' button should do double duty and also submit the step. If YES, the submit button
-    will not be shown, and the 'next' button will be enabled or not depending on whether submission is enabled.
+    Whether the 'Next Page' button should do double duty and also submit the step. Only valid if this is a terminal
+    step on the page.
+    
+    If NO, the Next Page button will not be enabled until the user submits the step and no new step is branched to. If
+    YES, the submit button will be hidden, and the 'Next Page' button will be enabled or not according to whether step
+    submission is enabled.
+    
+    @property {Boolean}
   */
-  nextButtonShouldSubmit: SC.Record.attr(Boolean)
+  nextButtonShouldSubmit: SC.Record.attr(Boolean),
+  
+  /**
+    Not currently implemented. 
+    
+    If we are the last step, whether to automatically skip to the next page when this step finishes. Different from
+    nextButtonShouldSubmit because (nominally) it leaves the submit button visible and the next button hidden; unsure
+    if we want to implement both this and nextButtonShouldSubmit
+  */
+  shouldAutoAdvancePage: SC.Record.attr(Boolean)
 
 }) ;
