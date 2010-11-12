970816bc413c8823ff9d7600c87b98c91b97fd43
Richard Klancer <rpk@pobox.com>
2010-11-12 12:10:45 -0500

    Document ACTIVITY_STEP & ACTIVITY_STEP_SUBMITTED

--- apps/smartgraphs/states/activity_step.js
+++ apps/smartgraphs/states/activity_step.js
@@ -5,16 +5,17 @@
 // ==========================================================================
 /*globals Smartgraphs */
 
+sc_require('states/activity');
+
 /** @class
 
-  State representing that an ActivityStep is currently active. Defines most of the commands available to an activity
-  step.
+  State representing that an ActivityStep is currently active and has not yet been submitted.
+  
+  This state defines most of the commands available to an activity author.
   
   @extends SC.Responder
   @version 0.1
 */
-
-sc_require('states/activity');
 
 Smartgraphs.ACTIVITY_STEP = SC.Responder.create(
 /** @scope Smartgraphs.ACTIVITY_STEP.prototype */ {
@@ -33,35 +34,124 @@ Smartgraphs.ACTIVITY_STEP = SC.Responder.create(
   // ACTIONS
   // 
   
+  /**
+    Sets the configuration of the panes on the right-hand side. Options are "single" or "split".
+    
+    This configuration affects the 'pane' parameters of further commands in the following way. If the current pane
+    configuration is 'single', the only valid 'pane' value is 'single'. If the pane configuration is split,
+    the value of 'pane' can be 'top' or 'bottom'.
+    
+    @param context
+    @param args
+    
+    @param {String} args.pane
+      The desired pane configuration. If "single", a single pane will be shown on the right. If "split", the right
+      side will be split into equally-sized top and bottom panes.
+  */
   setPaneConfig: function (context, args) {
     Smartgraphs.activityViewController.setPaneConfig(args);
     return YES;
   },
+  
+  /**
+    Make the referenced pane display an empty view.
     
+    @param context
+    @param args
+    
+    @param {String} args.pane
+      The pane to be hidden. Valid values are 'single', 'top', or 'bottom'. 
+  */
   hidePane: function (context, args) {
     Smartgraphs.activityViewController.hidePane(args);
     return YES;
   },
 
+  /**
+    Show an image in the referenced pane.
+  
+    @param context
+    @param args
+    
+    @param {String} args.pane
+      The pane in which to show the image. Valid values are 'single', 'top', or 'bottom'.
+    
+    @param {String} args.path
+      The url at which to find the image.
+  */
   showImage: function (context, args) {
     return Smartgraphs.activityViewController.showImage(args.pane, args.path);
   },
   
+  /**
+    Show a graph in the specified pane.
+    
+    @param context  
+    @param args
+    
+    @param {String} args.pane
+      The pane in which to show the graph. Valid values are 'single', 'top', or 'bottom'.
+    
+    @param {String} args.name
+      The name of the graph to show. A graph with this name must be defined elsewhere in the activity.
+  */
   showGraph: function (context, args) {
     Smartgraphs.activityViewController.showGraph(args.pane, args.name);
     return YES;
   },
   
+  /**
+    Show a table in the specified pane corresponding to the dataset with the specified name.
+    
+    This command will wait for the specified graph to be open and for the specified dataset to be opened in that graph
+    before displaying the table. 
+    
+    In addition, when data is streamed into the dataset (for example, from the sensor applet) the table will switch to
+    'numeric display' mode, showing the values of the latest incoming datapoint in large type. When streaming stops,
+    it will automatically display the new data in a table form.
+    
+    @param context
+    @param args
+    
+    @param {String} args.pane
+      The pane in which to show the table. In practice, valid values are 'top', or 'bottom' because a graph must be
+      open while the table is showing.
+    
+    @param {String} args.graphName
+      Required. The name of a graph to which to link this table.
+    
+    @param {String} args.datasetName
+      The name of the Dataset to be displayed in the table.
+  */
   showTable: function (context, args) {
     Smartgraphs.activityViewController.showTable(args.pane, args.graphName, args.datasetName);  
   },
   
+  /**
+    Wait for the user to enter a response before progressing to the 'submitted' state.
+    
+    This command enables editing of the responseTemplate, if there is one.
+    
+    Moreover, If the current ActivityStep specifies a submissibilityInspector, this instantiates the inspector, which
+    watches the current state of the system as manipulated by the user and disables/enables submission accordingly.
+
+    If the current ActivityStep does not specify a submissibilityInspector, this command enables submission
+    immediately.
+    
+    This command executes automatically when an ActivityStep which has shouldWaitForSubmissibleResponse == YES begins.
+  */
   waitForResponse: function (context, args) {
     Smartgraphs.activityStepController.waitForResponse();
     Smartgraphs.responseTemplateController.set('editingShouldBeEnabled', YES);
     return YES;
   },
   
+  /**
+    If submission is enabled, transitions to the ACTIVITY_STEP_SUBMITTED state. Otherwise, does nothing.
+    
+    The transition to the ACTIVITY_STEP_SUBMITTED results in the execution of the 'afterSubmissionCommands' specified
+    by the current ACTIVITY_STEP and then either branches to the next step or ends the ActivityPage.
+  */
   submitStep: function () {
     if (Smartgraphs.activityStepController.get('canSubmit')) {
       Smartgraphs.makeFirstResponder(Smartgraphs.ACTIVITY_STEP_SUBMITTED);
@@ -69,6 +159,13 @@ Smartgraphs.ACTIVITY_STEP = SC.Responder.create(
     return YES;
   },
   
+  /**
+    Submits the current ActivityStep and opens the next ActivityPage. Note that this action will fail if the current
+    step is not in the submissible state.
+    
+    Once the step has been submitted, if the current step is not a terminal step (i.e., has isFinalStep == NO), the
+    action will fail and the system will remain in the ACTIVITY_STEP_SUBMITTED state.
+  */
   gotoNextPage: function () {
     this.submitStep();
     this.invokeLast(function () {
@@ -76,6 +173,19 @@ Smartgraphs.ACTIVITY_STEP = SC.Responder.create(
     });
   },
 
+  /**
+    Creates a new session-scoped Dataset on the specified graph.
+    
+    @param context
+    @param args
+    
+    @param {String} args.datasetName
+      The name to be given to the new Dataset.
+
+    @param {String} args.graphName
+      The graph on which the dataset will be shown when it is created. A graph with this name must be open in the 
+      page when this command executes.
+  */
   createDataset: function (context, args) {
     var dataset = Smartgraphs.sessionController.createDataset(args.datasetName);
     if (args.graphName) {
@@ -85,12 +195,43 @@ Smartgraphs.ACTIVITY_STEP = SC.Responder.create(
     return YES;
   },
   
+  /**
+    Remove the specified Dataset from the specified graph. 
+    
+    This does not delete the Dataset; it merely removes it from the graph.
+    
+    @param context
+    @param args
+    
+    @param {String} args.datasetName
+      The name of the dataset being displayed on the graph.
+
+    @param {String} args.graphName
+      The graph on which the dataset is being displayed. This graph must be open in the page.
+  */
   removeDataset: function (context, args) {
     var controller = Smartgraphs.GraphController.controllerForName[args.graphName];
     controller.removeDataset(args.datasetName);
     return YES;
   },
 
+  /**
+    Create an session-scoped Annotation with the specified type and name on the specified graph.
+    
+    This command creates an 'empty' Annotation. Subsequent commands can reference the annotation by name in order to
+    add content to it.
+    
+    @param context
+    @param args
+    
+    @param {String} args.type
+      The name of the subclass of Annotation to be created. For example, 'Smartgraphs.HighlightedPoint'
+    @param {String} args.name
+      The name to be given to the Annotation object once it is created.
+    @param {String} args.graphName
+      The name of the graph on which the Annotation should be displayed, once it is created. This graph must be open
+      in the page when this command executes.
+  */
   createAnnotation: function (context, args) {
     var controller = Smartgraphs.GraphController.controllerForName[args.graphName];
     var annotation = Smartgraphs.sessionController.createAnnotation(args.type, args.name);
@@ -98,18 +239,69 @@ Smartgraphs.ACTIVITY_STEP = SC.Responder.create(
     return YES;
   },
   
+  /**
+    Add an existing Annotation with the specified name and type to the specified graph.
+    
+    If no session-scoped Annotation with the specified name and type exists, an example Annotation with the specified
+    name and type is searched for and, if one is found, it is added to the graph.
+    
+    Note that eventually it will make sense to remove the requirement to specify the Annotation type as well as the 
+    Annotation name here. However, this requirement does exist now.
+    
+    @param context
+    @param args
+    
+    @param {String} args.type
+      The name of the subclass of Annotation. For example, 'Smartgraphs.HighlightedPoint'
+    @param {String} args.name
+      The name of the Annotation object.
+    @param {String} args.graphName
+      The name of the graph on which the Annotation should be displayed. This graph must be open in the page when this
+      command executes.
+  */
   addAnnotation: function (context, args) {
     var controller = Smartgraphs.GraphController.controllerForName[args.graphName];
     controller.addObjectByName(args.type, args.name);
     return YES;
   },
   
+  /** 
+    Remove the specified Annotation from the specified graph.
+    
+    @param context
+    @param args
+    
+    @param {String} args.name
+      The name of the Annotation to be removed from the graph.
+    @param {String} args.graphName
+      The name of the graph from which the Annotation should be removed. This graph must be open in the page when this
+      command executes.
+  */
   removeAnnotation: function (context, args) {
     var controller = Smartgraphs.GraphController.controllerForName[args.graphName];
     controller.removeAnnotation(args.name);
     return YES;
   },
   
+  /**
+    Begin 'freehand input' on the specified graph, for example to draw a prediction graph. The freehand input mode
+    enables the user to create an arbitrary sketch on the graph and turns on the 'clear' button which allows the user
+    to start their sketch over. The sketch is stored as a session-scoped FreehandSketch annotation with the specified
+    name, so that it can be called up later in the activity.
+    
+    The system remains in the FREEHAND_INPUT state or one of its substates until the user submits the step. It is
+    possible to specify a submissibilityInspector that only allows submission once an appropriate sketch has been 
+    completed.
+    
+    @param context
+    @param args
+    
+    @param {String} args.annotationName
+      The name to be given to the Smartgraphs.FreehandSketch annotation object which will hold the user's sketch.
+    @param {String} args.graphName
+      The name of the graph on which the user will draw (and on which the FreehandSketch annotation will be added.)
+      This graph must be open in the page when this command executes.
+  */
   startFreehandInput: function (context, args) {
     Smartgraphs.sendAction('createAnnotation', this, { 
       graphName: args.graphName,
@@ -124,6 +316,22 @@ Smartgraphs.ACTIVITY_STEP = SC.Responder.create(
     }
   },
   
+  /**
+    Allow sensor input into a Dataset with the provided name on the specified graph. This command starts up the sensor
+    applet and displays Start, Stop, and Clear buttons below the graph. (The Start button will be enabled when the
+    sensor applet indicates that it has started up and is ready to accept input from a connected probe. Actual
+    recording of data occurs only occurs after the user clicks the Start button.)
+    
+    The system remains in the SENSOR state or one of its substates until the user submits the step.
+    
+    @param context
+    @param args
+    
+    @param {String} args.datasetName
+      The name to be given to the Dataset which will hold the sensor-recorded data.
+    @param {String} args.graphName
+      The name of the graph on which the data will be shown.
+  */
   startSensorInput: function (context, args) {
     Smartgraphs.sendAction('createDataset', this, { 
       graphName: args.graphName, 
--- apps/smartgraphs/states/activity_step_submitted.js
+++ apps/smartgraphs/states/activity_step_submitted.js
@@ -5,13 +5,26 @@
 // ==========================================================================
 /*globals Smartgraphs */
 
+sc_require('states/activity');
+
 /** @class
   
+  State representing that the currently open ActivityStep has been submitted.
+  
+  In this state, the user's response is processed and evaluated, and the system may branch to a new step. (Transitions
+  between steps are not allowed in other states.) 
+  
+  A step's 'afterSubmissionCommands' are executed upon entry to this state, and then the activityStepController
+  examines the user's response and issues a gotoStep command appropriately.
+  
+  If no step is branched to while the activityStepController processes the user's response, the 'isFinalStep' property
+  is examined to determine if the current step is intended to be a terminal step. If this step is a terminal step in
+  the page, the system transitions to the ACTIVITY_PAGE_DONE state. If this step is not intended to be a terminal 
+  step and no step has been branched to, it is considered an error.
+
   @extends SC.Responder
   @version 0.1
 */
-
-sc_require('states/activity');
 
 Smartgraphs.ACTIVITY_STEP_SUBMITTED = SC.Responder.create(
 /** @scope Smartgraphs.ACTIVITY_STEP_SUBMITTED.prototype */ {
@@ -37,6 +50,19 @@ Smartgraphs.ACTIVITY_STEP_SUBMITTED = SC.Responder.create(
   // ACTIONS
   //
   
+  /**
+    Branch to the specified step. Transitions to the ACTIVITY_STEP_LOADING state after setting the
+    activityStepController's content to the new ActivityStep.
+    
+    Generally this command is called by the activityStepController when it processes the ResponseBranches and
+    defaultBranch property of the current step.
+    
+    @param context
+    @param args
+    
+    @param {String} args.stepId
+      The id (url) of the ActivityStep to branch to
+  */
   gotoStep: function (context, args) {
     var step = Smartgraphs.store.find(Smartgraphs.ActivityStep, args.stepId);
     Smartgraphs.activityStepController.set('content', step);
@@ -56,15 +82,23 @@ Smartgraphs.ACTIVITY_STEP_SUBMITTED = SC.Responder.create(
     Create a HighlightedPoint record with name highlightedPointName in the current session. Takes the point from the
     the selected datapoint in the dataset with name datasetName, which must be open in the graph graphName.
     
-    Does nothing if there is no dataset with the passed name, or it is not open in the graph with graphName, or 
-    there are more or fewer than 1 datapoints selected.
+    The highlightedPoint is not automatically added to the graph.
     
-    Arguments:
-      graphName
-      datasetName
-      highlightedPointName
-      
-    A possible variant of this command would not require a graphName to be specified.
+    This command does nothing if there is no dataset with the passed name, or it is not open in the graph with
+    graphName, or there are more or fewer than 1 datapoints selected.
+    
+    A possible future variant of this command would not require a graphName to be specified.
+        
+    @param context
+    @param args
+    
+    @param {String} args.graphName
+      The name of the graph on which the dataset with the selected point must be displayed. This graph must be open in 
+      the page when this command executes.
+    @param {String} args.datasetName
+      The name of the dataset, which must have a single selected point.
+    @param {String} args.highlightedPointName
+      The name given to the session-scoped HighlightedPoint annotation which will be created.
   */
   createHighlightedPointFromSelection: function (context, args) {
     // given the graphName, find the associated graph controller
