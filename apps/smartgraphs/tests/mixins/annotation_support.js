da69a4c07b337693fb635f6efa35701be2567d76
Parker Morse <flashesofpanic@gmail.com>
2010-11-21 17:37:44 -0500

    Unit tests for annotation_support mixin.

--- apps/smartgraphs/tests/mixins/annotation_support.js
+++ apps/smartgraphs/tests/mixins/annotation_support.js
@@ -2,14 +2,42 @@
 // Project:   Smartgraphs.annotationSupport Unit Test
 // Copyright: ©2010 My Company, Inc.
 // ==========================================================================
-/*globals Smartgraphs module test ok equals same stop start */
+/*globals Smartgraphs module test ok equals same stop start setup teardown */
 
-module("Smartgraphs.annotationSupport");
-
-// TODO: Replace with real unit test for Smartgraphs.annotationSupport
-test("test description", function() {
-  var expected = "test";
-  var result   = "test";
-  equals(result, expected, "test should equal test");
+// Because this is a mixin, we need to create an object to mix it in, in 
+// order to test
+Smartgraphs.AnnotationBucket = SC.Object.extend(Smartgraphs.AnnotationSupport, {
+  name: null
 });
 
+module("Smartgraphs.annotationSupport", {
+  setup: function() {
+    setup.store();
+  },
+  
+  teardown: function() {
+    teardown.store();
+  }
+});
+
+test("An object mixing in Annotation Support will respond to supportsAnnotations", function() {
+  expect(1);
+  var ab = Smartgraphs.AnnotationBucket.create({ 'name': "Test Bucket" });
+  var expected = YES;
+  var result   = ab.get("supportsAnnotations");
+  equals( result, expected, "the object should return YES to supportsAnnotations");
+});
+
+test("The annotationList array can be manipulated with addAnnotation, removeAnnotation", function() {
+  expect(3);
+  var ab = Smartgraphs.AnnotationBucket.create({ 'name': "Bucket Two" });
+  Smartgraphs.sessionController.newSession();
+  var hp = Smartgraphs.sessionController.createAnnotation(Smartgraphs.HighlightedPoint, "Test Point", { "color": "#cc0000" });
+  same( ab.get('annotationList'), null, "Annotation list is initially null");
+  ab.addAnnotation(hp);
+  equals( ab.get('annotationList').get('length'), 1, "The annotation list is 1");
+  ab.removeAnnotation("Test Point");
+  equals( ab.get('annotationList').get('length'), 0, "The annotation list is now empty (not null)");
+});
+
+// findAnnotationByName() is exercised by removeAnnotation(), above.
