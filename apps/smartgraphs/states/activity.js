3578da3a8ff3be60b34ee8ad84dda3c878d0578b
Parker Morse <flashesofpanic@gmail.com>
2010-12-01 17:33:32 -0500

    Add createRiseArrow and createRunArrow methods
    These go in the ACTIVITY state. They create the annotations but do not add them to the graph. Tests to come.

--- apps/smartgraphs/states/activity.js
+++ apps/smartgraphs/states/activity.js
@@ -59,6 +59,7 @@ Smartgraphs.ACTIVITY = SC.Responder.create(
       The RGB color definition for the color to render the line.
   */
   createLineThroughPoints: function (context, args) {
+    // TODO: This dependence on the controller is probably not useful long-term.
     // given the graphName, find the associated graph controller
     var controller = Smartgraphs.GraphController.controllerForName[args.graphName];
   
@@ -77,6 +78,96 @@ Smartgraphs.ACTIVITY = SC.Responder.create(
         color: color
       });
     return YES;
-  }
+  },
+
+  /** 
+    Create an Arrow annotation with the name arrowName in the current session. Takes two HighlightedPoints as the
+    defining points; the arrow will run from the height (y-value) of the first point vertically to the second
+    point.
+    
+    The Arrow is not automatically added to the graph.
+    
+    This method does nothing if there is no graph with the passed name.
+    
+    @param context
+    @param args
+    
+    @param {String} args.graphName
+      The name of the graph on which the dataset with the line must be displayed. This graph must be open in 
+      the page when this command executes.
+    @param {String} args.firstPoint
+      The name of one HighlightedPoint annotation.
+    @param {String} args.secondPoint
+      The name of the other HighlightedPoint annotation.      
+    @param {String} args.arrowName
+      The name given to the session-scoped Arrow annotation which will be created.
+    @param {String} args.color
+      The RGB color definition for the color to render the line.
+  */
+  createRiseArrow: function (context, args) {
+    // TODO: This dependence on the controller is probably not useful long-term.
+    // given the graphName, find the associated graph controller
+    var controller = Smartgraphs.GraphController.controllerForName[args.graphName];
   
+    if (!controller) return YES;
+    
+    var firstAnnotation = controller.findAnnotationByName(args.firstPoint);
+    var secondAnnotation = controller.findAnnotationByName(args.secondPoint);
+    var color = args.color ? args.color : "#000000";
+    
+    var riseArrow = 
+      Smartgraphs.sessionController.createAnnotation(Smartgraphs.Arrow, args.arrowName, { 
+        point1: firstAnnotation.get('point').get('id'),
+        point2: secondAnnotation.get('point').get('id'),
+        color: color,
+        isVertical: YES
+      });
+    return YES;
+  },
+
+  /** 
+    Create an Arrow annotation with the name arrowName in the current session. Takes two HighlightedPoints as the
+    defining points; the arrow will run from the first point horizontally to the x-value of the second
+    point.
+    
+    The Arrow is not automatically added to the graph.
+    
+    This method does nothing if there is no graph with the passed name.
+    
+    @param context
+    @param args
+    
+    @param {String} args.graphName
+      The name of the graph on which the dataset with the line must be displayed. This graph must be open in 
+      the page when this command executes.
+    @param {String} args.firstPoint
+      The name of one HighlightedPoint annotation.
+    @param {String} args.secondPoint
+      The name of the other HighlightedPoint annotation.      
+    @param {String} args.arrowName
+      The name given to the session-scoped Arrow annotation which will be created.
+    @param {String} args.color
+      The RGB color definition for the color to render the line.
+  */
+  createRunArrow: function (context, args) {
+    // TODO: This dependence on the controller is probably not useful long-term.
+    // given the graphName, find the associated graph controller
+    var controller = Smartgraphs.GraphController.controllerForName[args.graphName];
+  
+    if (!controller) return YES;
+    
+    var firstAnnotation = controller.findAnnotationByName(args.firstPoint);
+    var secondAnnotation = controller.findAnnotationByName(args.secondPoint);
+    var color = args.color ? args.color : "#000000";
+    
+    var runArrow = 
+      Smartgraphs.sessionController.createAnnotation(Smartgraphs.Arrow, args.arrowName, { 
+        point1: firstAnnotation.get('point').get('id'),
+        point2: secondAnnotation.get('point').get('id'),
+        color: color,
+        isHorizontal: YES
+      });
+    return YES;
+  }
+
 }) ;
