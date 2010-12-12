d9d9594aadcbf9523ced2c4fba2731627586ed1d
Richard Klancer <rpk@pobox.com>
2010-12-12 17:11:32 -0500

    fix AUTHOR tests for statechart

--- apps/smartgraphs/tests/states/author.js
+++ apps/smartgraphs/tests/states/author.js
@@ -9,13 +9,22 @@ var dummyState = SC.Responder.create();
 
 module("Smartgraphs.AUTHOR", {
   setup: function () {
-    setup.mock(Smartgraphs, 'READY', dummyState);
-    setup.mock(Smartgraphs.AUTHOR, 'nextResponder', Smartgraphs.READY);
+    
+    setup.mock(Smartgraphs, 'statechart', SC.Statechart.create({
+      trace: YES,
+      rootState: SC.State.design({
+        initialSubstate: 'DUMMY',
+        DUMMY: SC.State.design(),
+        LOADING_ACTIVITY: SC.State.design(),
+        AUTHOR: SC.State.plugin('Smartgraphs.AUTHOR')
+      })
+    }));
+    
+    Smartgraphs.statechart.initStatechart();
     setup.store();
   },
   
   teardown: function () {
-    Smartgraphs.makeFirstResponder(Smartgraphs.READY);
     teardown.all();
   }
 });
@@ -23,7 +32,7 @@ module("Smartgraphs.AUTHOR", {
 test("AUTHOR should open the author view", function () {
   expect(1);
   Smartgraphs.appWindowController.set('viewToShow', null);
-  Smartgraphs.makeFirstResponder(Smartgraphs.AUTHOR);
+  Smartgraphs.statechart.gotoState('AUTHOR');
   equals(Smartgraphs.appWindowController.get('viewToShow'), 'Smartgraphs.authorPage.authorView', "The activity view should be open after transitioning to AUTHOR state");
 });
 
@@ -33,7 +42,7 @@ test("entering AUTHOR state should set selectability and navigability", function
   Smartgraphs.activityOutlineController.set('isSelectable', NO);
   Smartgraphs.activityViewController.set('enableBackAndForward', NO);
   
-  Smartgraphs.makeFirstResponder(Smartgraphs.AUTHOR);
+  Smartgraphs.statechart.gotoState('AUTHOR');
   
   equals( Smartgraphs.activityOutlineController.get('isSelectable'), YES, "Transitioning to AUTHOR state should set 'isSelectable' on outline controller to YES");
   equals( Smartgraphs.activityViewController.get('enableBackAndForward'), YES, "Transitioning to AUTHOR state should set 'enableBackAndForward' on activity view controller to YES");  
@@ -42,12 +51,12 @@ test("entering AUTHOR state should set selectability and navigability", function
 
 test("exiting AUTHOR state should unset selectability and navigability", function () {
   expect(2);
-  Smartgraphs.makeFirstResponder(Smartgraphs.AUTHOR);
+  Smartgraphs.statechart.gotoState('AUTHOR');
   // if they aren't already...
   Smartgraphs.activityOutlineController.set('isSelectable', YES);
   Smartgraphs.activityViewController.set('enableBackAndForward', YES);
   
-  Smartgraphs.makeFirstResponder(Smartgraphs.READY);
+  Smartgraphs.statechart.gotoState('DUMMY');
   
   equals( Smartgraphs.activityOutlineController.get('isSelectable'), NO, "Transitioning away from AUTHOR state should set 'isSelectable' on outline controller to NO");
   equals( Smartgraphs.activityViewController.get('enableBackAndForward'), NO, "Transitioning away from AUTHOR state should set 'enableBackAndForward' on activity view controller to NO");  
@@ -56,14 +65,11 @@ test("exiting AUTHOR state should unset selectability and navigability", functio
 
 test("'runActivity' action in AUTHOR state should go to LOADING_ACTIVITY and set openAuthorViewAfterLoading to NO", function () {
   expect(2);
-  setup.mock(Smartgraphs.LOADING_ACTIVITY, 'nextResponder', Smartgraphs.READY);
-  setup.mock(Smartgraphs.LOADING_ACTIVITY, 'didBecomeFirstResponder', function () {});
-  setup.mock(Smartgraphs.LOADING_ACTIVITY, 'willLoseFirstResponder', function () {});
-  Smartgraphs.LOADING_ACTIVITY.set('openAuthorViewAfterLoading', YES);
-
-  Smartgraphs.makeFirstResponder(Smartgraphs.AUTHOR);
-  Smartgraphs.sendAction('runActivity');
   
-  equals(Smartgraphs.get('firstResponder'), Smartgraphs.LOADING_ACTIVITY, "after 'runActivity' action, Smartgraphs should be in the LOADING_ACTIVITY state");
-  equals(Smartgraphs.LOADING_ACTIVITY.get('openAuthorViewAfterLoading'), NO, "after 'runActivity' action in AUTHOR state, LOADING_ACTIVITY.openAuthorViewAfterLoading should be NO");
+  setup.mock(Smartgraphs.loadingActivityController, 'openAuthorViewAfterLoading', YES);
+  Smartgraphs.statechart.gotoState('AUTHOR');
+  Smartgraphs.statechart.sendAction('runActivity');
+
+  same(Smartgraphs.statechart.get('currentStates').getEach('name'), ['LOADING_ACTIVITY'], "after 'runActivity' action, Smartgraphs should be in the LOADING_ACTIVITY state");
+  equals(Smartgraphs.loadingActivityController.get('openAuthorViewAfterLoading'), NO, "after 'runActivity' action in AUTHOR state, LOADING_ACTIVITY.openAuthorViewAfterLoading should be NO");
 });
