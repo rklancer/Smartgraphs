4207451919b3a6a774cc20cb84d900357dc5928e
Richard Klancer <rpk@pobox.com>
2010-12-17 16:58:24 -0500

    Improved activityObjectsController

--- apps/smartgraphs/controllers/activity_objects.js
+++ apps/smartgraphs/controllers/activity_objects.js
@@ -20,46 +20,49 @@ Smartgraphs.activityObjectsController = SC.ObjectController.create(
 /** @scope Smartgraphs.activityObjectsController.prototype */ {
   
   /**
-    All annotations available to the user session, indexed by name. Observable.
+    @private
+    
+    All annotations available to the user session, indexed by name.
   */
-  annotations: SC.Object.create(),
+  _annotations: {},
   
   /**
-    All datasets avaiable to the user session, indexed by name. Observable.
+    @private
+    
+    All datasets available to the user session, indexed by name.
   */
-  datasets: SC.Object.create(),
+  _datasets: {},
   
   /**
-    Call this when starting a session to set the list of annotations and datasets to those predefined by the activity
-    document.
+    Sets the registry of dataset and annotation in this activity to be just those predefined in the activity document.
+    (i.e., removes any datasets or annotations dynamically created in a run of the activity.
   */
   loadPredefinedObjects: function () {
-    var datasets = SC.Object.create();
-    var annotations = SC.Object.create();
-    
-    // FIXME it is probably more efficient to make this query a computed property of the activity
+    this._datasets = {};
+    this._annotations = {};
+      
     var query = SC.Query.local(Smartgraphs.Dataset, 'activity={activity}', {
-      activity: Smartgraphs.activityController.get('content')
+      activity: Smartgraphs.activityController.get('activityRecordInCurrentStore')
     });
-    var foundDatasets = Smartgraphs.store.find(query).refresh();
+    var foundDatasets = Smartgraphs.store.find(query);
     
     if ( !(foundDatasets.get('status') & SC.Record.READY)) {
       throw "predefined dataset records are not READY!";
     }
-    
+    var self = this;
     foundDatasets.forEach(function (dataset) {
       var name = dataset.get('name');
-      if (datasets.get(name)) {
+      if (self._datasets[name]) {
         throw "The activity contains multiple datasets named '%@'".fmt(name);
       }
-      datasets.set(name, dataset);
+      self._datasets[name] = dataset;
     });
     
     // now, repeat the above for each annotation type...
     
     Smartgraphs.Annotation.types.forEach(function (type) {
       query = SC.Query.local(type, 'activity={activity}', {
-        activity: Smartgraphs.activityController.get('content')
+        activity: Smartgraphs.activityController.get('activityRecordInCurrentStore')
       });
       var foundAnnotations = Smartgraphs.store.find(query).refresh();
       
@@ -69,54 +72,81 @@ Smartgraphs.activityObjectsController = SC.ObjectController.create(
 
       foundAnnotations.forEach(function (annotation) {
         var name = annotation.get('name');
-        if (annotations.get(name)) {
+        if (self._annotations[name]) {
           throw "The activity contains multiple datasets named '%@'".fmt(name);
         }        
-        annotation.set(name, annotation);
+        self._annotations[name] = annotation;
       });
     });
     
-    this.set('datasets', datasets);
-    this.set('annotations', annotations);
+    this.notifyPropertyChange('datasetNames');
+    this.notifyPropertyChange('annotationNames');
   },
   
   findDataset: function (name) {
-    return this.get('datasets').get(name);
+    // FIXME this is not a good way to do this. Name collisions with object properties are inevitable.
+    return this._datasets[name];
   },
   
   findAnnotation: function (name) {
-    return this.get('annotations').get(name);
+    return this._annotations[name];
   },
   
   createDataset: function (name) {
-    if (this.findDataset(name)) {
+    if (this._datasets[name]) {
       throw "The activity tried to create a dataset with name %@, which is already in use.".fmt(name);
     }
     
     var dataset = Smartgraphs.store.createRecord(Smartgraphs.Dataset, { 
-      activity: Smartgraphs.activityController.get('url'),
+      activity: Smartgraphs.activityController.get('id'),
       name: name,
       points: []
     });
     dataset.set('id', Smartgraphs.getNextGuid());
     
-    this.get('datasets').set(name, dataset);
+    this._datasets[name] = dataset;
+    this.notifyPropertyChange('datasetNames');
     return dataset;
   },
   
   createAnnotation: function (type, name, attributes) {
-    if (this.findAnnotation(name)) {
+    if (this._annotations[name]) {
       throw "The activity tried to create an annotation with name %@, which is already in use.".fmt(name);
     }
     
     var annotation = Smartgraphs.store.createRecord(type, SC.mixin({
-      activity: Smartgraphs.activityController.get('url'),
+      activity: Smartgraphs.activityController.get('id'),
       name: name
     }, attributes));
     annotation.set('id', Smartgraphs.getNextGuid());
 
-    this.get('annotations').set(name, annotation);
+    this._annotations[name] = annotation;
+    this.notifyPropertyChange('annotationNames');
     return annotation;
-  }
+  },
+  
+  /**
+    Observable list of the names of all datasets available to the current activity. (Observers are notified whenever
+    datasets are added or removed from this list.)
+  */
+  datasetNames: function () {
+    var names = [];
+    for (var name in this._datasets) {
+      if (this._datasets.hasOwnProperty(name)) names.push(name);
+    }
+    return names;
+  }.property(),
+  
+  /**
+    Observable list of the names of all annotations available to the current activity. (Observers are notified 
+    whenever annotations are added or removed from this list.)
+  */
+  annotationNames: function () {
+    var names = [];
+    for (var name in this._annotations) {
+      if (this._annotations.hasOwnProperty(name)) names.push(name);
+    }
+    return names;
+  }.property()
   
 }) ;
