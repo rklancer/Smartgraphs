b664cb0052c6cf9da2379b3317f9e4080056be4c
Richard Klancer <rpk@pobox.com>
2010-12-22 19:12:46 -0500

    Update datasource to commit record!

--- apps/smartgraphs/data_sources/couch.js
+++ apps/smartgraphs/data_sources/couch.js
@@ -153,12 +153,113 @@ Smartgraphs.CouchDataSource = SC.DataSource.extend(
     return NO ; // return YES if you handled the storeKey
   },
   
+  markRecordBusy: function (store, record) {
+    var K = SC.Record;
+    var storeKey = record.get('storeKey');
+    
+    if (record.get('store') !== store) throw "WHOOPS. Trying to mark a record busy that's from the wrong store";
+    
+    var status = store.readStatus(record.get('storeKey'));
+    
+    // copied from SC.Store.commitRecords
+    if ((status == K.EMPTY) || (status == K.ERROR)) {
+      throw K.NOT_FOUND_ERROR ;
+    } 
+    else {
+      if(status==K.READY_NEW) {
+        store.writeStatus(storeKey, K.BUSY_CREATING);
+        store.dataHashDidChange(storeKey, null, YES);
+      } 
+      else if (status==K.READY_DIRTY) {
+        store.writeStatus(storeKey, K.BUSY_COMMITTING);
+        this.log("marking record '%s' as BUSY_COMMITTING: ", record.toString());
+        store.dataHashDidChange(storeKey, null, YES);
+      } 
+      else if (status==K.DESTROYED_DIRTY) {
+        store.writeStatus(storeKey, K.BUSY_DESTROYING);
+        store.dataHashDidChange(storeKey, null, YES);
+      } 
+      else if (status==K.DESTROYED_CLEAN) {
+        store.dataHashDidChange(storeKey, null, YES);
+      }
+      // ignore K.READY_CLEAN, K.BUSY_LOADING, K.BUSY_CREATING, K.BUSY_COMMITTING, 
+      // K.BUSY_REFRESH_CLEAN, K_BUSY_REFRESH_DIRTY, KBUSY_DESTROYING
+    }
+  },
+  
+  markRecordCommitted: function (store, record) {
+    var K = SC.Record;
+    var storeKey = record.get('storeKey');
+    var status = store.readStatus(storeKey);
+    
+    if (status === K.BUSY_DESTROYING) {
+      store.dataSourceDidDestroy(storeKey);
+    }
+    else if (status & K.BUSY) {
+      store.dataSourceDidComplete(storeKey);
+    }
+    // ignore everything else!
+  },
+  
+  applyToChildRecords: function (store, record, fn) {
+    var self = this;
+
+    fn.call(this, store, record);
+
+    for (var prop in record) {
+      if (SC.kindOf(record[prop], SC.SingleAttribute) || SC.kindOf(record[prop], SC.ManyAttribute)) {
+        var type = record[prop].type;
+        var inverse = record[prop].inverse;
+        if (type && inverse && SC.objectForPropertyPath(type).prototype[inverse].aggregate) {
+          var relatedRecords = record.get(prop);
+          if ( !relatedRecords.get('isSCArray')) relatedRecords = [relatedRecords];
+          relatedRecords.forEach(function (relatedRecord) { self.applyToChildRecords(store, relatedRecord, fn); } );
+        }
+      }
+    }
+  },
+  
   updateRecord: function(store, storeKey) {
     
-    // TODO: Add handlers to submit modified record to the data source
-    // call store.dataSourceDidComplete(storeKey) when done.
+    // if we downloaded it, we can upload it
+    if (this._ids[storeKey]) {
+      var record = store.find(store.recordTypeFor(storeKey), store.idFor(storeKey));
 
-    return NO ; // return YES if you handled the storeKey
+      if (record && record.serialize) {
+        var doc = record.serialize();
+        doc._rev = this._revs[storeKey];
+        doc.data_format_version = Smartgraphs.DATA_FORMAT_VERSION;
+        
+        // this is a bit obscure at the moment, but it locks 'child' records of the activity (set status to BUSY)
+        // until the xhr callback completes.
+
+        this.applyToChildRecords(store, record, this.markRecordBusy);
+        
+        SC.Request.putUrl('/db/smartgraphs/'+this._ids[storeKey])
+                  .json()
+                  .header('Accept', 'application/json')
+                  .notify(this, this.didUpdateActivity, store, storeKey)
+                  .send(doc);
+        return YES;
+      }
+    }
+
+    return NO ;
+  },
+
+  didUpdateActivity: function (response, store, storeKey) {
+    var K = SC.Record;
+    
+    var record = store.find(store.recordTypeFor(storeKey), store.idFor(storeKey));
+    
+    if (SC.ok(response)) {
+      this.applyToChildRecords(store, record, this.markRecordCommitted);
+      this._revs[storeKey] = response.get('body').rev;
+    }
+    else {
+      //FIXME also iterate over child records!
+      store.dataSourceDidError(storeKey);
+    }
   },
   
   destroyRecord: function(store, storeKey) {
