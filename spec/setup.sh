f3fdefaa8137acf4f0b921c6dbf8aa4036ff8320
Richard Klancer <rpk@pobox.com>
2011-01-11 18:03:43 -0800

    Update spec/setup.sh with shell var for password

--- spec/setup.sh
+++ spec/setup.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-# Pass CouchDB username/password as first & second arguments. Call from project root.
+# Call from project root. Pass CouchDB username as first argument. Password should be in $COUCH_PASSWORD.
 
 # drop test activities
-curl -s http://127.0.0.1:5984/smartgraphs/_design/app/_list/urls-with-revs/revs-of-test-activities | xargs -n 1 printf "http://%s:%s@127.0.0.1:5984/smartgraphs/%s\n" $1 $2 | xargs curl -vX DELETE
+curl -s http://127.0.0.1:5984/smartgraphs/_design/app/_list/urls-with-revs/revs-of-test-activities | xargs -n 1 printf "http://%s:%s@127.0.0.1:5984/smartgraphs/%s\n" $1 $COUCH_PASSWORD | xargs curl -vX DELETE
 
 # reset test activities
 for name in `ls apps/smartgraphs/activity_json/test/*.json` ; do
