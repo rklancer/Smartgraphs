b6e9c61a4597a02941ee5958a03ebc70d398bd17
Richard Klancer <rpk@pobox.com>
2011-01-11 17:48:04 -0800

    Fixes for test activity setup script

--- spec/setup.sh
+++ spec/setup.sh
@@ -3,9 +3,9 @@
 # Pass CouchDB username/password as first & second arguments. Call from project root.
 
 # drop test activities
-curl -s http://127.0.0.1:5984/smartgraphs/_design/app/_list/urls-with-revs/revs-of-test-activities | xargs printf "http://%s:%s@127.0.0.1:5984/smartgraphs/%s\n" $1 $2 | xargs -n 1 curl -X DELETE
+curl -s http://127.0.0.1:5984/smartgraphs/_design/app/_list/urls-with-revs/revs-of-test-activities | xargs -n 1 printf "http://%s:%s@127.0.0.1:5984/smartgraphs/%s\n" $1 $2 | xargs curl -vX DELETE
 
 # reset test activities
 for name in `ls apps/smartgraphs/activity_json/test/*.json` ; do
-  printf http://127.0.0.1:5984/smartgraphs/%s `basename $name` | xargs -n 1 curl -X PUT -d@$name
+  printf http://127.0.0.1:5984/smartgraphs/%s `basename $name` | xargs -n 1 curl -vX PUT -d@$name
 done
