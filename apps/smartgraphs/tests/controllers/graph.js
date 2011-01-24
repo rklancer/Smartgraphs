1ef98141cdce6cf7699769af37c4b28bff737dd0
Richard Klancer <rpk@pobox.com>
2011-01-24 17:41:26 -0500

    Test that addDataset checks units

--- apps/smartgraphs/tests/controllers/graph.js
+++ apps/smartgraphs/tests/controllers/graph.js
@@ -14,12 +14,30 @@ function setupGraphFixtures() {
   Smartgraphs.Dataset.oldFixtures = Smartgraphs.Dataset.FIXTURES;
   Smartgraphs.Dataset.FIXTURES = [{url: 'dataset-1'}];
   
+  Smartgraphs.Axis.oldFixtures = Smartgraphs.Axis.FIXTURES;
+  Smartgraphs.Axis.FIXTURES = [
+    { url: 'x-axis',
+      min: -5,
+      max: 10,
+      nSteps: 5,
+      label: 'x axis'
+    },
+    { url: 'y-axis',
+      min: 2,
+      max: 8,
+      nSteps: 6,
+      label: 'y axis'
+    }
+  ];
+  
   Smartgraphs.Graph.oldFixtures = Smartgraphs.Graph.FIXTURES;  
   Smartgraphs.Graph.FIXTURES = [
     { url: 'test',
       name: 'test',
       title: 'Test Graph',
-      initialDatasets: [dataset1.get('name')]
+      xAxis: 'x-axis',
+      yAxis: 'y-axis',
+      initialDatasets: ['test-dataset-1']
     }
   ];
 }
@@ -27,6 +45,7 @@ function setupGraphFixtures() {
 function restoreGraphFixtures() {
   Smartgraphs.Graph.FIXTURES = Smartgraphs.Graph.oldFixtures;
   Smartgraphs.Dataset.FIXTURES = Smartgraphs.Dataset.oldFixtures;
+  Smartgraphs.Axis.FIXTURES = Smartgraphs.Axis.oldFixtures;  
 }
 
 
@@ -36,16 +55,22 @@ module("Smartgraphs.graphController", {
     Smartgraphs.set('store', SC.Store.create().from(SC.FixturesDataSource.create()));
     
     setupUserAndSessionFixtures();
+    setupGraphFixtures();
+    
+    Smartgraphs.store.find(Smartgraphs.Unit);
+    Smartgraphs.store.find(Smartgraphs.Axis);
+    Smartgraphs.store.find(Smartgraphs.Dataset);    
+    Smartgraphs.store.find(Smartgraphs.Graph);
+
     beginSession();
    
     dataset1 = Smartgraphs.activityObjectsController.createDataset('test-dataset-1');
     dataset2 = Smartgraphs.activityObjectsController.createDataset('test-dataset-2');
     dataset3 = Smartgraphs.activityObjectsController.createDataset('test-dataset-3');
-       
-    setupGraphFixtures();
   },
   
   teardown: function () {
+    Smartgraphs.firstGraphController.clear();
     endSession();
 
     restoreUserAndSessionFixtures();
@@ -77,5 +102,50 @@ test("adding dataset to a graph results in assignment of different colors to 'co
   ok( color3 !== color2, "dataset3 and dataset2 should have different colors");
   ok( color3 !== color1, "dataset3 and dataset1 should have different colors");
 });
+
+
+test("Graph controller refuses to add a dataset of the wrong unit type", function () {
+  expect(5);
+  Smartgraphs.firstGraphController.openGraph('test');
+  
+  var meters = Smartgraphs.store.find(Smartgraphs.Unit, '/builtins/units/meters');
+  var seconds = Smartgraphs.store.find(Smartgraphs.Unit, '/builtins/units/seconds');
+  var degrees = Smartgraphs.store.find(Smartgraphs.Unit, '/builtins/units/degrees-celsius');
+  var minutes = Smartgraphs.store.find(Smartgraphs.Unit, '/builtins/units/minutes');
+  
+  var xAxis = Smartgraphs.firstGraphController.get('xAxis');
+  var yAxis = Smartgraphs.firstGraphController.get('yAxis');
+    
+  xAxis.set('units', seconds);
+  yAxis.set('units', meters);
+  
+  var startingLength = Smartgraphs.firstGraphController.getPath('datasetList.length');
+  
+  dataset2.set('xUnits', null);
+  dataset2.set('yUnits', meters);
+  Smartgraphs.firstGraphController.addDataset(dataset2);
+  equals( Smartgraphs.firstGraphController.getPath('datasetList.length'), startingLength, "graph controller should refuse to add dataset with mismatching units null & meters");
+  
+  dataset2.set('xUnits', minutes);
+  dataset2.set('yUnits', meters);
+  Smartgraphs.firstGraphController.addDataset(dataset2);
+  equals( Smartgraphs.firstGraphController.getPath('datasetList.length'), startingLength, "graph controller should refuse to add dataset with mismatching units minutes & meters");
+  
+  dataset2.set('xUnits', seconds);
+  dataset2.set('yUnits', null);
+  Smartgraphs.firstGraphController.addDataset(dataset2);
+  equals( Smartgraphs.firstGraphController.getPath('datasetList.length'), startingLength, "graph controller should refuse to add dataset with mismatching units seconds & null");
+  
+  dataset2.set('xUnits', seconds);
+  dataset2.set('yUnits', degrees);
+  Smartgraphs.firstGraphController.addDataset(dataset2);
+  equals( Smartgraphs.firstGraphController.getPath('datasetList.length'), startingLength, "graph controller should refuse to add dataset with mismatching units seconds & degrees C");
+    
+  dataset2.set('xUnits', seconds);
+  dataset2.set('yUnits', meters);
+  Smartgraphs.firstGraphController.addDataset(dataset2);
+  equals( Smartgraphs.firstGraphController.getPath('datasetList.length'), startingLength + 1, "graph controller should add dataset when units match (seconds and meters)");
+});
+
 // This test exercises most of the controller.
 // TODO: include an annotation.
