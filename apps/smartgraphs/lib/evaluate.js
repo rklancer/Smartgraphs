3c830da198c5de43cfac5f1329ade99d8a518464
Richard Klancer <rpk@pobox.com>
2011-02-15 13:25:08 -0500

    remove 'DummyInspector' entirely

--- apps/smartgraphs/lib/evaluate.js
+++ apps/smartgraphs/lib/evaluate.js
@@ -203,11 +203,7 @@ sc_require('lib/evaluator');
     return num.length > 0 && !isNaN(num);
   }
   
-  evaluate = function (exp, value) {
-    if (value === Smartgraphs.DummyInspector.token) {
-      return Smartgraphs.evaluator.evaluate(exp);
-    }
-    
+  evaluate = function (exp, value) {  
     if (exp === 'value') return value;
     
     if (exp === undefined || exp === null || typeof(exp) === 'string' || typeof(exp) === 'number' || typeof(exp) === 'boolean' || exp.splice === [].splice ) { 
