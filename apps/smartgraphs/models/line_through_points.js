c1b08bf5f59086c0d435b8c440ab4ed0d829bf1e
Richard Klancer <rpk@pobox.com>
2011-02-18 16:10:31 -0500

    Declaratively specify LineThroughPoints w/transform handler

--- apps/smartgraphs/models/line_through_points.js
+++ apps/smartgraphs/models/line_through_points.js
@@ -18,24 +18,53 @@ sc_require('views/line_through_points');
   @version 0.1
 */
 
-Smartgraphs.LineThroughPoints = Smartgraphs.Annotation.extend(
-/** @scope Smartgraphs.LineThroughPoints.prototype */ {
-
-  /**
-    The first of the two points which define (but do not limit) the line.
+(function () {
+  
+  
+  // FIXME generalize this transform and put it somewhere for use by other record types!
+  
+  var transform = { 
+    /** @private - convert the datahash value to a DataPoint record:
+       (1) by dereferencing it as a DataPoint id, if it looks like a url (starts with '/')
+       (2) by finding an annotation with the specified name and dereferencing its 'point' property, if it looks like a name
+   */  
+    to: function(idOrName, attr, recordType, parentRecord) {
+      var annotation, point;
     
-    @property {Smartgraphs.DataPoint}
-  */
-  point1: SC.Record.toOne('Smartgraphs.DataPoint'),
+      if (SC.none(idOrName) || (idOrName==="")) return null;
 
-  /**
-    The second of the two points which define (but do not limit) the line.
+      if (idOrName[0] === "/") {
+        return parentRecord.get('store').find(recordType, idOrName);
+      }
+      annotation = Smartgraphs.activityObjectsController.findAnnotation(idOrName);
+      point = annotation && annotation.get('point');
+      return point || null;
+    },
+
+    /** @private - convert a record instance to a record id */
+    from: function(record) { return record ? record.get('id') : null; }
+  };
+
+  Smartgraphs.LineThroughPoints = Smartgraphs.Annotation.extend(
+  /** @scope Smartgraphs.LineThroughPoints.prototype */ {
+
+    /**
+      The first of the two points which define (but do not limit) the line.
     
-    @property {Smartgraphs.DataPoint}
-  */
-  point2: SC.Record.toOne('Smartgraphs.DataPoint')
+      @property {Smartgraphs.DataPoint}
+    */
+    point1: SC.Record.toOne('Smartgraphs.DataPoint', { transform: transform }),
+  
+    /**
+      The second of the two points which define (but do not limit) the line.
+    
+      @property {Smartgraphs.DataPoint}
+    */
+    point2: SC.Record.toOne('Smartgraphs.DataPoint', { transform: transform })
 
-}) ;
+  }) ;
 
-// let the graph view know how to instantiate a view class to display this item
-Smartgraphs.LineThroughPoints.viewClass = Smartgraphs.LineThroughPointsView;
+  // let the graph view know how to instantiate a view class to display this item
+  Smartgraphs.LineThroughPoints.viewClass = Smartgraphs.LineThroughPointsView;
+
+}());
