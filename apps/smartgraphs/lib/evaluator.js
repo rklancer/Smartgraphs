ac6ac3f6b72055ee4bd5fcc156bf114308e16b75
Richard Klancer <rpk@pobox.com>
2011-03-02 13:24:20 -0500

    Split expression definition into separate file

--- apps/smartgraphs/lib/evaluator.js
+++ apps/smartgraphs/lib/evaluator.js
@@ -3,7 +3,7 @@
 // Copyright: ©2011 Concord Consortium
 // Author:    Richard Klancer <rpk@pobox.com>
 // ==========================================================================
-/*globals Smartgraphs def */
+/*globals Smartgraphs */
 
 Smartgraphs.evaluator = {
   
@@ -27,18 +27,14 @@ Smartgraphs.evaluator = {
     return op;
   },
   
-  // define many expression operators, see below for usage
-  defs: function (defsCallback) {
-    var self = this,
-        oldDef = window.def;
+  // define many expression operators, see lib/expressions.js for usage
+  defineExpressions: function (defsCallback) {
+    var def, self = this;
     
-    window.def = function () {
+    def = function () {
       return self.def.apply(self, arguments);
-    };    
-
-    defsCallback();
-
-    window.def = oldDef;
+    };
+    defsCallback(def);
   },
   
   _args: function (nArgs) {
@@ -164,181 +160,3 @@ Smartgraphs.evaluator = {
   }
   
 };
-
-
-Smartgraphs.evaluator.defs( function () {  
-  
-  def('+', function () {
-    var ret = 0;
-    for (var i = 0; i < arguments.length; i++) {
-      ret += arguments[i];
-    }
-    return ret;
-  }).minArgs(2);
-
-
-  def('-', function () {
-    var ret = arguments[0];
-    for (var i = 1; i < arguments.length; i++) {
-      ret -= arguments[i];
-    }
-    return ret;
-  }).minArgs(2);
-
-
-  def('absDiff', function (x, y) {
-    return Math.abs(x - y); 
-  }).args(2);
-
-
-  def('=', function (x, y) {
-    return x === y;
-  }).args(2);
-
-
-  def('indexOf', function (name) {
-    var annotation = Smartgraphs.activityObjectsController.findAnnotation(name);
-    if (!annotation) throw "Annotation " + name + " not found.";
-    return annotation.getPath('point.dataset.points').indexOf(annotation.get('point'));
-  }).args(1);
-
-
-  def('isNumeric', function (val) {
-    // see http://stackoverflow.com/questions/18082/validate-numbers-in-javascript-isnumeric/1830844#1830844
-    return !isNaN(parseFloat(val)) && isFinite(val);  
-  }).args(1);
-  
-  
-  // get a response field using 1-based index of response fields
-  def('responseField', function (index) {
-    var values = Smartgraphs.responseTemplateController.get('values');
-    return values[index - 1];
-  }).args(1).dependsOn('Smartgraphs.responseTemplateController*values.[]');
-
-
-  def('coord', function (axis, annotationName) {
-    var annotation = Smartgraphs.activityObjectsController.findAnnotation(annotationName);
-  
-    if (axis !== 'x' && axis !== 'y') throw "x or y coordinates only!"
-    if (!annotation) throw "Annotation " + annotationName + " not found.";
-    if (!annotation.get('point')) throw "Annotation " + annotationName + " does not have a 'point' property";
-
-    return annotation.get('point').get(axis);
-  }).args(2);
-
-
-  def('slope', function (name1, name2) {
-    var anno1 = Smartgraphs.activityObjectsController.findAnnotation(name1);
-    var anno2 = Smartgraphs.activityObjectsController.findAnnotation(name2);
-    var p1 = anno1.get('point');
-    var p2 = anno2.get('point');
-  
-    return (p1.get('y') - p2.get('y')) / (p1.get('x') - p2.get('x'));
-  }).args(2);
-
-
-  def('withinAbsTolerance', function (val1, val2, tolerance) {
-    return Math.abs(val1 - val2) < tolerance;
-  }).args(3);
-
-
-  def('slopeToolOrder', function (name1, name2) {
-    var anno1 = Smartgraphs.activityObjectsController.findAnnotation(name1);
-    var anno2 = Smartgraphs.activityObjectsController.findAnnotation(name2);
-    var p1 = anno1.get('point');
-    var p2 = anno2.get('point');
-  
-    // currently, "slope tool order" means points go from left to right
-    return p1.get('x') < p2.get('x') ? [name1, name2] : [name2, name1];
-  }).args(2);
-
-
-  def('delta', function (axis, namePair) {
-    var anno1 = Smartgraphs.activityObjectsController.findAnnotation(namePair[0]);
-    var anno2 = Smartgraphs.activityObjectsController.findAnnotation(namePair[1]);
-    var p1 = anno1.get('point');
-    var p2 = anno2.get('point');
-  
-    return p2.get(axis) - p1.get(axis);
-  }).args(2);
-
-
-  def('or', function () {
-    for (var i = 0; i < arguments.length; i++) {
-      if (arguments[i]) return true;
-    }
-    return false;
-  }).minArgs(1);
-
-
-  def('not', function (val) {
-    return !val;
-  }).args(1);
-
-
-  def('samePoint', function (name1, name2) {
-    var anno1 = Smartgraphs.activityObjectsController.findAnnotation(name1);
-    var anno2 = Smartgraphs.activityObjectsController.findAnnotation(name2);
-    var p1 = anno1.get('point');
-    var p2 = anno2.get('point');
-  
-    return p1 && p1 === p2;
-  }).args(2);
-
-
-  // note this (textLengthIsAtLeast 1 (responseField 0)) is easier to describe in an expression-authoring interface 
-  // than is (> 0 (strip (responseField 0)))
-
-  def('textLengthIsAtLeast', function (minLength, text) {
-    return (text || '').strip().length >= minLength;
-  }).args(2);
-
-
-  def('coord', function (axis, annotationName) {
-    var annotation = Smartgraphs.activityObjectsController.findAnnotation(annotationName),
-        point = annotation.get('point');
-    return point.get(axis);
-  }).args(2);
-
-
-  def('max', function () {
-    return Math.max.apply(Math, arguments);
-  }).minArgs(1);
-
-
-  def('min', function () {
-    return Math.min.apply(Math, arguments);
-  }).minArgs(1);
-
-
-  def('abs', function (val) {
-    return Math.abs(val);
-  }).args(1);
-
-
-  // note that quantities should carry units with them, not require unit pluralization to be written into a variable
-  // by the author!
-  def('pluralizeUnits', function (unitId, quantity) {
-    var units = Smartgraphs.store.find(Smartgraphs.Unit, unitId);
-    if (!units) throw "Couldn't find units '%@'".fmt(unitId);
-    return units.pluralizeFor(quantity);
-  }).args(2);
-
-
-  // dereference a single variable. May want to implement this in the evaluator itself, but first we need to demonstrate
-  // a clear need to do so, and consistent rules;  we are NOT trying to implement Scheme in javascript
-  def('get', function (name) {
-    return Smartgraphs.activityPageController.getFromContext(name);
-  }).args(1);
-
-
-  def('/', function (x, y) {
-    return x / y;
-  }).args(2);
-
-
-  def('listItem', function (index, list) {
-    return list[index-1];
-  }).args(2);
-
-});
