8be41114839f1b53aa35a2754ff5049c8a7a69c4
Richard Klancer <rpk@pobox.com>
2011-03-04 12:15:34 -0500

    Remove now-unneeded binding transform from DataPoint

--- apps/smartgraphs/models/data_point.js
+++ apps/smartgraphs/models/data_point.js
@@ -28,25 +28,3 @@ Smartgraphs.DataPoint = SC.Record.extend(
   }.property('y').cacheable()
 
 });
-
-
-SC.RecordAttribute.registerTransform(Smartgraphs.DataPoint, { 
-  /** @private - convert the datahash value to a DataPoint record:
-     (1) by dereferencing it as a DataPoint id, if it looks like a url (starts with '/')
-     (2) by finding an annotation with the specified name and dereferencing its 'point' property, if it looks like a name
- */  
-  to: function(idOrName, attr, recordType, parentRecord) {
-    var annotation, point;
-  
-    if (SC.none(idOrName) || (idOrName==="")) return null;
-    annotation = Smartgraphs.activityObjectsController.findAnnotation(idOrName);
-    point = annotation && annotation.get('point');
-    if (point) return point;
-
-    point = Smartgraphs.store.find(recordType, idOrName);
-    return point || null;
-  },
-
-  /** @private - convert a record instance to a record id */
-  from: function(record) { return record ? record.get('id') : null; }
-});
