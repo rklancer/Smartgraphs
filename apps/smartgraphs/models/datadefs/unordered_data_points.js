44d4560ec4882dd98c3ea21bba334c42712db8b7
Richard Klancer <rpk@pobox.com>
2011-03-30 13:34:27 -0400

    UnorderedDataPoints model

---
+++ apps/smartgraphs/models/datadefs/unordered_data_points.js
@@ -0,0 +1,23 @@
+// ==========================================================================
+// Project:   Smartgraphs.UnorderedDataPoints
+// Copyright: ©2011 Concord Consortium
+// Author:    Richard Klancer <rpk@pobox.com>
+// ==========================================================================
+/*globals Smartgraphs */
+
+sc_require('models/datadef');
+
+/** @class
+
+  The UnorderedDataPoints data definition class defines a dataset that is described by an unordered set of (x,y)
+  pairs.
+  
+  @extends Smartgraphs.Datadef
+  @version 0.1
+*/
+Smartgraphs.UnorderedDataPoints = Smartgraphs.Datadef.extend(
+/** @scope Smartgraphs.UnorderedDataPoints.prototype */ {
+  
+  points: SC.Record.attr(Array)
+  
+});
