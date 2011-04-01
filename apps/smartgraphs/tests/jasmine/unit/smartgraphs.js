d9187001d1622d83c0d3c2fc39eb00fc24bc0dbe
Richard Klancer <rpk@pobox.com>
2011-04-01 14:37:59 -0400

    Add tests/jasmine and move qunit tests to tests/qunit

---
+++ apps/smartgraphs/tests/jasmine/unit/smartgraphs.js
@@ -0,0 +1,7 @@
+/*globals Smartgraphs describe it expect xit xdescribe beforeEach afterEach spyOn runs waits waitsFor clickOn fillIn */
+
+describe("Smartgraphs", function () {
+  it("should be defined", function () {
+    expect(Smartgraphs).toBeDefined();
+  });
+});
