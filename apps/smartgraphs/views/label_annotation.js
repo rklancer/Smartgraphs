2457882785264581e6364289cffff6451489d41c
Richard Klancer <rpk@pobox.com>
2011-05-17 09:58:21 -0400

    Make label view text Raphael text

--- apps/smartgraphs/views/label_annotation.js
+++ apps/smartgraphs/views/label_annotation.js
@@ -14,18 +14,7 @@
 */
 Smartgraphs.LabelAnnotationView = RaphaelViews.RaphaelView.extend(
 /** @scope Smartgraphs.LabelAnnotationView.prototype */ {
-  
-  init: function () {
-    var labelTextView = this.get('labelTextViewDesign').create();
     
-    sc_super();
-    
-    labelTextView.set('labelView', this);
-    this.set('labelTextView', labelTextView);
-
-    if (this.get('parentView')) this.parentViewDidChange();
-  },
-  
   // this is overwritten if we're added to the graphView directly
   item: function () {
     return this.get('content');
@@ -37,11 +26,7 @@ Smartgraphs.LabelAnnotationView = RaphaelViews.RaphaelView.extend(
   }.property().cacheable(),
   
   textBinding: '*item.text',
-  textColor: '#333333',
-  
-  textDidChange: function () {
-    this.get('labelTextView').updateText();
-  }.observes('text', 'textColor'),
+  textColor:   '#333333',
   
   shouldMarkTargetPointBinding: '*item.shouldMarkTargetPoint',
   shouldMarkTargetPointBindingDefault: SC.Binding.oneWay(),
@@ -87,103 +72,15 @@ Smartgraphs.LabelAnnotationView = RaphaelViews.RaphaelView.extend(
         
     // need to calculate an offset more intelligently, but for now...
     
-    this.set('bodyXCoord', xCoord + xOffset);
-    this.set('bodyYCoord', yCoord + yOffset - height);
+    this.set('bodyXCoord',   xCoord + xOffset);
+    this.set('bodyYCoord',   yCoord + yOffset - height);
     this.set('anchorXCoord', xCoord + xOffset + width / 2);
     this.set('anchorYCoord', yCoord + yOffset);
-
-    this.get('labelTextView').updateLayout();
-        
+            
   }.observes('xCoord', 'yCoord', 'xOffset', 'yOffset', 'width', 'height'),
 
   childViews: 'targetPointView connectingLineView labelBodyView'.w(),
-  
-  /**
-    This is not a childView of the labelView; it's not part of the RaphaelView hierarchy. Instead, it's a normal
-    HTML view that gets added to or removed from the graphView, and moved around, as needed as the labelView is moved.
-  */
-  labelTextViewDesign: SC.LabelView.design({
     
-    // set by labelView when it inits. We can't use 'parentView' to get the label view because our parentView is
-    // actually the graphView 
-    labelView: null,
-    
-    labelBodyView: SC.outlet('labelView.labelBodyView'),
-    
-    // implementing any bindings to labelView properties here cause jasmine tests to choke badly once more than a few are run...
-    // (so we'll use these update methods and observers instead)
-    
-    init: function () {
-      sc_super();
-      this.set('cursor', SC.Cursor.create());
-    },
-    
-    updateText: function () {
-      this.set('value', this.getPath('labelView.text'));
-    },
-    
-    // forward drag-related events to the labelBodyView
-    mouseDown: function (evt) {
-      this.get('labelBodyView').startDrag(evt);
-      return YES;     // returning NO means we don't get notified of mouseUp
-    },
-    
-    mouseDragged: function (evt) {
-      this.get('labelBodyView').drag(evt);
-    },
-    
-    mouseUp: function (evt) {
-      this.get('labelBodyView').endDrag(evt);
-      return NO;      // returning YES means we don't get notified of doubleClick
-    },
-    
-    doubleClick: function () {
-      return YES;
-    },
-
-    updateLayout: function () {
-      var labelView = this.get('labelView');
-      
-      this.adjust('width',  labelView.get('width')      - 20);
-      this.adjust('height', labelView.get('height')     - 10);
-      this.adjust('top',    labelView.get('bodyYCoord') + 5);
-      this.adjust('left',   labelView.get('bodyXCoord') + 10);
-
-      // mysteries: why don't the layout changes cause this particular view to re-render? Why doesn't updateLayer()
-      // do the trick?
-      this.replaceLayer();
-    }
-  }),
-  
-  /**
-    Add the labelTextView (which is not a Raphael view and not part of this view hierarchy) to our graphView if we
-    were added to the graph view's view hierarchy
-  */
-  parentViewDidChange: function () {
-    sc_super();
-    var labelTextView = this.get('labelTextView');
-    
-    sc_super();
-    
-    if (this.get('parentView')) {
-      this.get('graphView').appendChild(labelTextView);
-    }
-    else {
-      this.get('graphView').removeChild(labelTextView);
-    }
-  },
-  
-  render: function () {
-    sc_super();
-    var self = this;
-    this.invokeLast( function () {
-      self.beginPropertyChanges();
-      self.notifyPropertyChange('xCoord');
-      self.notifyPropertyChange('yCoord');
-      self.endPropertyChanges();
-    });
-  },
-  
   targetPointView: RaphaelViews.RaphaelView.design({
     displayProperties: 'xCoord yCoord stroke'.w(),
     
@@ -325,23 +222,28 @@ Smartgraphs.LabelAnnotationView = RaphaelViews.RaphaelView.extend(
   
   labelBodyView: RaphaelViews.RaphaelView.design({
     
+    childViews: ['labelTextView'],
+    
     labelView: SC.outlet('parentView'),
-    labelTextView: SC.outlet('labelView.labelTextView'),
     
     displayProperties: 'bodyXCoord bodyYCoord width height stroke strokeWidth fill cornerRadius'.w(),
     
-    bodyXCoordBinding: '.labelView.bodyXCoord',
-    bodyYCoordBinding: '.labelView.bodyYCoord',
-    xOffsetBinding: '.labelView.xOffset',
-    yOffsetBinding: '.labelView.yOffset',
-    widthBinding: '.labelView.width',
-    heightBinding: '.labelView.height',
+    bodyXCoordBinding:   '.labelView.bodyXCoord',
+    bodyYCoordBinding:   '.labelView.bodyYCoord',
+    xOffsetBinding:      '.labelView.xOffset',
+    yOffsetBinding:      '.labelView.yOffset',
+    widthBinding:        '.labelView.width',
+    heightBinding:       '.labelView.height',
 
-    defaultStrokeBinding: '.labelView.stroke',
+    fillBinding:         '.labelView.fill',
+    cornerRadiusBinding: '.labelView.cornerRadius',
+    
+    defaultStrokeBinding:     '.labelView.stroke',
     highlightedStrokeBinding: '.labelView.highlightedStroke',
-    defaultStrokeWidth: 1,
-    highlightedStrokeWidth: 2,
-    isHighlightedBinding: '.labelView.isBodyDragging',
+    defaultStrokeWidth:       1,
+    highlightedStrokeWidth:   2,
+    
+    isHighlightedBinding:     '.labelView.isBodyDragging',
     
     stroke: function () {
       return this.get('isHighlighted') ? this.get('highlightedStroke') : this.get('defaultStroke');
@@ -351,45 +253,79 @@ Smartgraphs.LabelAnnotationView = RaphaelViews.RaphaelView.extend(
       return this.get('isHighlighted') ? this.get('highlightedStrokeWidth') : this.get('defaultStrokeWidth');
     }.property('isHighlighted', 'highlightedStrokeWidth', 'defaultStrokeWidth').cacheable(),
     
-    fillBinding: '.parentView.fill',
-    cornerRadiusBinding: '.parentView.cornerRadius',
     
     renderCallback: function (raphaelCanvas, attrs) {
       return raphaelCanvas.rect().attr(attrs);
     },
     
     render: function (context, firstTime) {      
-      var width        = this.get('width') || 0,
-          height       = this.get('height') || 0,
-          bodyXCoord   = this.get('bodyXCoord') || 0,
-          bodyYCoord   = this.get('bodyYCoord') || 0,
-          cornerRadius = this.get('cornerRadius') || 0,
-          stroke       = this.get('stroke'),
-          strokeWidth  = this.get('strokeWidth'),
-          fill         = this.get('fill'),       
-
-          attrs = {
-            x: bodyXCoord,
-            y: bodyYCoord,
-            width: width,
-            height: height,
-            stroke: stroke,
-            'stroke-width': strokeWidth,
-            fill: fill,
-            'fill-opacity': 1.0,
-            r: cornerRadius
+      var attrs = {
+            x:              this.get('bodyXCoord') || 0,
+            y:              this.get('bodyYCoord') || 0,            
+            width:          this.get('width') || 0,
+            height:         this.get('height') || 0,
+            r:              this.get('cornerRadius') || 0,          
+            stroke:         this.get('stroke'),
+            'stroke-width': this.get('strokeWidth'),
+            fill:           this.get('fill'),
+            'fill-opacity': 1.0
           },
 
           raphaelRect;
       
       if (firstTime) {
         context.callback(this, this.renderCallback, attrs);
+        this.renderChildViews(context, firstTime);        
       }
       else {
         raphaelRect = this.get('raphaelObject');
         raphaelRect.attr(attrs);
       }
-    },      
+    },
+    
+    labelTextView: RaphaelViews.RaphaelView.design({
+      
+      displayProperties: 'text textColor width height bodyXCoord bodyYCoord'.w(),
+      
+      labelView:     SC.outlet('parentView.labelView'),
+      labelBodyView: SC.outlet('parentView'),
+      
+      textBinding:       '.labelView.text',
+      textColorBinding:  '.labelView.textColor',  
+      widthBinding:      '.labelBodyView.width',
+      heightBinding:     '.labelBodyView.height',
+      bodyXCoordBinding: '.labelBodyView.bodyXCoord',
+      bodyYCoordBinding: '.labelBodyView.bodyYCoord',
+      
+      renderCallback: function (raphaelCanvas, attrs) {
+        return raphaelCanvas.text().attr(attrs);
+      },
+      
+      render: function (context, firstTime) {
+        var height = this.get('height'),
+            
+            attrs = {
+              text:          this.get('text'),
+              width:         this.get('width') - 20,
+              height:        height - 10,
+              x:             this.get('bodyXCoord') + 10,
+              y:             this.get('bodyYCoord') + height/2,
+              fill:          this.get('textColor'),
+              'font-size':   12,
+              'text-anchor': 'start'
+            },
+
+            raphaelText;
+            
+        if (firstTime) {
+          context.callback(this, this.renderCallback, attrs);
+        }
+        else {
+          raphaelText = this.get('raphaelObject');
+          raphaelText.attr(attrs);
+        }   
+      }
+    }),
     
     // Dragging. Note that dragging is 'stateless' in the sense that you can always drag a label view. So we won't hook
     // into states or the controller layer. We also assume until proven otherwise that we can modify our own cursor
@@ -416,7 +352,6 @@ Smartgraphs.LabelAnnotationView = RaphaelViews.RaphaelView.extend(
 
       // our layer doesn't respect SC.Cursor, so set the cursors manually
       this.$().css('cursor', 'move');
-      this.setPath('labelTextView.cursor.cursorStyle', 'move');     // get the labelTextView to show the same cursor
     },
 
     drag: function (evt) {
@@ -437,7 +372,6 @@ Smartgraphs.LabelAnnotationView = RaphaelViews.RaphaelView.extend(
       this._isDragging = NO;
       
       this.$().css('cursor', 'default');
-      this.setPath('labelTextView.cursor.cursorStyle', 'default'); 
     }
     
   })
