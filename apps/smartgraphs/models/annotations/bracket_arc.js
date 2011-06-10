192baeda8272bd47dbde323eab25e6eddfeae4a3
Richard Klancer <rpk@pobox.com>
2011-06-10 10:34:34 -0400

    Fix Brackets for datadefs

--- apps/smartgraphs/models/annotations/bracket_arc.js
+++ apps/smartgraphs/models/annotations/bracket_arc.js
@@ -24,6 +24,12 @@ sc_require('views/bracket_arc');
 Smartgraphs.BracketArc = Smartgraphs.Annotation.extend(
 /** @scope Smartgraphs.BracketArc.prototype */ {
 
+  datadefName: SC.Record.attr(String),
+  
+  datadef: function () {
+    return Smartgraphs.activityObjectsController.findDatadef(this.get('datadefName'));
+  }.property('datadefName').cacheable(),
+  
   /**
     The index in the table of the point the bracket should start at.
     
@@ -44,69 +50,90 @@ Smartgraphs.BracketArc = Smartgraphs.Annotation.extend(
     @property {Boolean}
   */
   isLeftOfColumn: SC.Record.attr(Boolean),
-  
-  //p1: Smartgraphs.Tag.pointFromTag('p1Record', 'p1Tag'),
-  
-  //p2: Smartgraphs.Tag.pointFromTag('p2Record', 'p2Tag'),
-  
-  /**
-    Optionally, item1Index and item2Index can be derived from 2 DataPoints. This is one of those points.
-    
-    @property {Smartgraphs.DataPoint}
-  */
-  p1Record: SC.Record.toOne('Smartgraphs.DataPoint'),
-  
-  /**
-    Optionally, item1Index and item2Index can be derived from 2 DataPoints. This is one of those points.
-    
-    @property {Smartgraphs.DataPoint}
-  */
-  p2Record: SC.Record.toOne('Smartgraphs.DataPoint'),
 
   /**
-    Optional Tag object which can be used to indirectly specify p1
-    (TODO: change this from a HighlightedPoint to a Tag)
+    x-coordinate of start point
 
-    @property {Smartgraphs.Tag}
+    @property {Number}
   */
+  x1Record: SC.Record.attr(Number),
+
+  /**
+    y-coordinate of start point
+    
+    @property {Number}
+  */
+  y1Record: SC.Record.attr(Number),
+  
+  /**
+    x-coordinate of end point
+    
+    @property {Number}
+  */
+  x2Record: SC.Record.attr(Number),
+
+  /**
+    y-coordinate of end point
+
+    @property {Number}
+  */
+  y2Record: SC.Record.attr(Number),
+  
   p1Tag: SC.Record.toOne('Smartgraphs.Tag'),
-
-  /**
-    Optional Tag object which can be used to indirectly specify p2
-    (TODO: change this from a HighlightedPoint to a Tag)
-
-    @property {Smartgraphs.Tag}
-  */
+  
   p2Tag: SC.Record.toOne('Smartgraphs.Tag'),
   
+  p1x: Smartgraphs.Tag.valueFrom('p1Tag', 'x', 'x1Record'),
+  
+  p1y: Smartgraphs.Tag.valueFrom('p1Tag', 'y', 'y1Record'),
+
+  p2x: Smartgraphs.Tag.valueFrom('p2Tag', 'x', 'x2Record'),
+
+  p2y: Smartgraphs.Tag.valueFrom('p2Tag', 'y', 'y2Record'),
+  
   _pointIndicesDidChange: function () {
-    var p1 = this.get('p1'),
-        p2 = this.get('p2'),
+    var p1x = this.get('p1x'),
+        p1y = this.get('p1y'),
+        p2x = this.get('p2x'),
+        p2y = this.get('p2y'),
         indices;
 
-    if (!p1 || !p2) return;    
-    if (SC.none(p1.get('dataset')) || (p1.get('dataset') !== p2.get('dataset'))) return;
+    if (SC.none(this.get('datadef'))) return;
+    if (SC.none(p1x) || SC.none(p1y) || SC.none(p2x) || SC.none(p2y)) return;
     
-    indices = this.calculateIndicesFromPoints(p1, p2);
+    indices = this.calculateIndicesFromPoints(p1x, p1y, p2x, p2y);
     
     this.setIfChanged('item1Index', indices.item1Index);
     this.setIfChanged('item2Index', indices.item2Index);
-  }.observes('*p1.dataset.points.[]', 'p1', 'p2'),  
+  }.observes('*datadef.points.[]', 'p1x', 'p1y', 'p2x', 'p2y'),
   
   /**
     Calculate item1Index and item2Index from (p1, p2). The base class implementation sets item1Index to the index of 
     p1 in the 'points' array of the dataset containing p1 and p2; and sets item2Index to the index of p2.
   */
-  calculateIndicesFromPoints: function (p1, p2) {
-    var points = p1.getPath('dataset.points') || [];
+  calculateIndicesFromPoints: function (p1x, p1y, p2x, p2y) {
+    var points = this.getPath('datadef.points') || [],
+        i,
+        len,
+        point,
+        item1Index,
+        item2Index;
+        
+    points = points.map( function (pair) { return pair.copy(); } ).sort( function (pair1, pair2) { return pair1[0] - pair2[0]; } );
+        
+    for (i = 0, len = points.get('length'); i < len; i++) {
+      point = points.objectAt(i);
+      if (point[0] === p1x && point[1] === p1y) item1Index = i;
+      if (point[0] === p2x && point[1] === p2y) item2Index = i;
+    }
     
     return {
-      item1Index: points.indexOf(p1),
-      item2Index: points.indexOf(p2)
+      item1Index: item1Index || -1,
+      item2Index: item2Index || -1
     };
   }
   
-}) ;
+});
 
 // let the graph view know how to instantiate a view class to display this item
 Smartgraphs.BracketArc.viewClass = Smartgraphs.BracketArcView;
--- apps/smartgraphs/models/annotations/rise_bracket.js
+++ apps/smartgraphs/models/annotations/rise_bracket.js
@@ -19,19 +19,17 @@ Smartgraphs.RiseBracket = Smartgraphs.BracketArc.extend(
   
   /**
     Calculate item1Index and item2Index from (p1, p2). This uses "slope tool order" in which arrows are drawn from the 
-    leftmost to the rightmost point. Because 'points' is automatically ordered by x-value, this means item1Index is
+    leftmost to the rightmost point. When 'points' is rdered by x-value, this means item1Index is
     the lesser of the indices of p1 and p2 in the points array of the dataset containing p1 and p2; and item2Index is
     the greater of the indices of p1 and p2
   */
-  calculateIndicesFromPoints: function (p1, p2) {
-    var points = p1.getPath('dataset.points') || [],
-        p1Index = points.indexOf(p1),
-        p2Index = points.indexOf(p2);
-    
+  calculateIndicesFromPoints: function (p1x, p1y, p2x, p2y) {
+    var items = sc_super();
+        
     return {
-      item1Index: Math.min(p1Index, p2Index),
-      item2Index: Math.max(p1Index, p2Index)
-    };
+      item1Index: Math.min(items.item1Index, items.item2Index),
+      item2Index: Math.max(items.item1Index, items.item2Index)
+    }
   }
 
 });
