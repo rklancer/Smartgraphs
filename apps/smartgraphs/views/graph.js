aa930bd85949d95a9da9b77139fe59041c55aa19
Richard Klancer <rpk@pobox.com>
2011-07-01 16:54:53 -0400

    Move mouse-event transparency into OverlayAnnotationsHolder

--- apps/smartgraphs/views/graph.js
+++ apps/smartgraphs/views/graph.js
@@ -611,8 +611,48 @@ Smartgraphs.GraphView = SC.View.extend(
     annotationsHolder: RaphaelViews.RaphaelView.design({
     }),
     
-    // Holds the 'overlay annotations' and, crucially, attempts to be transparent to mouse events (TODO)
+    // Holds the 'overlay annotations'; is transparent to mouse events
     overlayAnnotationsHolder: RaphaelViews.RaphaelView.design({
+
+      // In order to be transparent to mouse events, we weant to intercept mousemove, mousedown, and mouseup events at 
+      // the DOM level, so that we can trick the SproutCore root responder into thinking the events actually happened
+      // on the non-overlay views beneath us (see handleEvent)
+      didCreateLayer: function () {
+        var self = this;
+
+        this.$().mousemove(function (evt) {
+          self.handleEvent(evt);
+        });
+
+        this.$().mousedown(function (evt) {
+          self.handleEvent(evt);
+        });
+
+        this.$().mouseup(function (evt) {
+          self.handleEvent(evt);
+        });
+      },
+
+      handleEvent: function (evt) {
+        // Stop propagation. If we let the mousemove event bubble, the SproutCore root responder will think we were the 
+        // "last hovered" view, which screws up its calculation of hover (i.e., mouseEntered and mouseExited) events for
+        // any views below us.
+        evt.stopPropagation();
+
+        // Find the element UNDER us at the location of the mouse event
+        this.$().hide();
+        var el = document.elementFromPoint(evt.clientX, evt.clientY);     // should work in IE!
+        this.$().show();
+
+        // Set the event target to be the element beneath us. Because 'event' is a jQuery-normalized event, 'target' is a 
+        // normal R/W property
+        evt.target = el;
+
+        // NOW let SproutCore think the event happened directly to the element below us. It will handle forwarding 
+        // mouseDown, mouseMoved, mouseExited, mouseEntered events to the SC.Views beneath us.
+        SC.Event.handle.call(document, evt);
+      }
+      
     }),
 
     // Holds the animation channel. Should be later in the DOM (and thus "in front of") the annotation views.
--- apps/smartgraphs/views/segment_overlay.js
+++ apps/smartgraphs/views/segment_overlay.js
@@ -101,46 +101,6 @@ Smartgraphs.SegmentOverlayView = RaphaelViews.RaphaelView.extend(
       path = context.raphael();
       path.attr(attrs);
     }
-  },
-  
-  // TODO pull this 'overlay' functionality out into some kind of mixin
-  
-  // We want to intercept mousemove, mousedown, and mouseup events at the DOM level, so that we can trick the 
-  // SproutCore root responder into thinking the events actually happened on the views beneath us (see handleEvent)
-  didCreateLayer: function () {
-    var self = this;
-    
-    this.$().mousemove(function (evt) {
-      self.handleEvent(evt);
-    });
-    
-    this.$().mousedown(function (evt) {
-      self.handleEvent(evt);
-    });
-    
-    this.$().mouseup(function (evt) {
-      self.handleEvent(evt);
-    });
-  },
-
-  handleEvent: function (evt) {
-    // Stop propagation. If we let the mousemove event bubble, the SproutCore root responder will think we were the 
-    // "last hovered" view, which screws up its calculation of hover (i.e., mouseEntered and mouseExited) events for
-    // any views below us.
-    evt.stopPropagation();
-    
-    // Find the element UNDER us at the location of the mouse event
-    this.$().hide();
-    var el = document.elementFromPoint(evt.clientX, evt.clientY);     // should work in IE!
-    this.$().show();
-    
-    // Set the event target to be the element beneath us. Because 'event' is a jQuery-normalized event, 'target' is a 
-    // normal R/W property
-    evt.target = el;
-    
-    // NOW let SproutCore think the event happened directly to the element below us. It will handle forwarding 
-    // mouseDown, mouseMoved, mouseExited, mouseEntered events to the SC.Views beneath us.
-    SC.Event.handle.call(document, evt);
   }
-  
+
 });
