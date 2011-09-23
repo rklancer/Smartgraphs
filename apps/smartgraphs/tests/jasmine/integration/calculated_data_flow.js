12b159d6255b7fc1b098ba0e5a8dabc4034e9ad7
Richard Klancer <rpk@pobox.com>
2011-09-23 14:55:36 -0400

    Start testing Pointsets too. Ready to start breaking stuff.

--- apps/smartgraphs/tests/jasmine/integration/calculated_data_flow.js
+++ apps/smartgraphs/tests/jasmine/integration/calculated_data_flow.js
@@ -1,39 +1,47 @@
 (function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   defineJasmineHelpers();
   describe("Data flow from UnorderedDataPoints record to GraphableObjects", function() {
     var store, udpRecord;
     udpRecord = store = null;
     beforeEach(function() {
+      console.log("making a new store and a new udpRecord");
       store = SC.Store.create().from(SC.FixturesDataSource.create());
       udpRecord = store.createRecord(Smartgraphs.UnorderedDataPoints, {
-        url: "udp-record"
+        url: "udp-record",
+        points: []
       });
-      return this.addMatchers({
-        toContainPair: function(p) {
-          var a, _i, _len, _ref;
-          _ref = this.actual;
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            a = _ref[_i];
-            return true;
-          }
-        },
-        toEqualPairs: function(ps) {
-          var a, i, p, _i, _len, _ref;
-          if (!(this.actual != null) || ps.length !== this.actual.length) {
-            return false;
-          }
-          i = 0;
-          _ref = this.actual;
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            a = _ref[_i];
-            p = ps[i++];
-            if (a[0] !== p[0] || a[1] !== p[1]) {
+      return __bind(function() {
+        var toMatchUsing;
+        toMatchUsing = function(matcher) {
+          return function(pairs) {
+            var a, i, _len, _ref, _ref2;
+            if (((_ref = this.actual) != null ? _ref.length : void 0) !== pairs.length || typeof this.actual !== 'object') {
               return false;
             }
-          }
-          return true;
-        }
-      });
+            _ref2 = this.actual;
+            for (i = 0, _len = _ref2.length; i < _len; i++) {
+              a = _ref2[i];
+              if (!matcher(a, pairs[i])) {
+                return false;
+              }
+            }
+            return true;
+          };
+        };
+        return this.addMatchers({
+          toEqualPairs: toMatchUsing(function(a, _arg) {
+            var x, y;
+            x = _arg[0], y = _arg[1];
+            return a[0] === x && a[1] === y;
+          }),
+          toEqualPointsDefinedBy: toMatchUsing(function(a, _arg) {
+            var x, y;
+            x = _arg[0], y = _arg[1];
+            return a.get('x') === x && a.get('y') === y;
+          })
+        });
+      }, this)();
     });
     return describe("when the source UnorderedDataPoint's 'points' array is empty", function() {
       var dataRepresentation, line, pointset;
@@ -60,8 +68,9 @@
       });
       return describe("and the source UnorderedDataPoints record's 'points' array is then populated with [0,1] and [2,3]", function() {
         var sourcePoints;
-        sourcePoints = [[0, 1], [2, 3]];
+        sourcePoints = null;
         beforeEach(function() {
+          sourcePoints = [[0, 1], [2, 3]];
           return udpRecord.set('points', sourcePoints);
         });
         describe("the line's points array", function() {
@@ -70,12 +79,12 @@
           beforeEach(function() {
             return points = line.get('points');
           });
-          it("should have the two pairs [0,1] and [2,3] in its points array", function() {
+          it("should have the two pairs [0,1] and [2,3]", function() {
             expect(points).toContain([0, 1]);
             expect(points).toContain([2, 3]);
             return expect(points.length).toEqual(2);
           });
-          return describe("a representative point taken from the line's points array", function() {
+          return describe("a representative point", function() {
             var point;
             point = null;
             beforeEach(function() {
@@ -87,18 +96,41 @@
             });
           });
         });
+        describe("the poinset's points array", function() {
+          var points;
+          points = null;
+          beforeEach(function() {
+            return points = pointset.get('points');
+          });
+          return it("should have the Smartgraph.Points [0,1] and [2,3]", function() {
+            return expect(points).toEqualPointsDefinedBy([[0, 1], [2, 3]]);
+          });
+        });
         return describe("and a point (4,5) is added to the UnorderedDataPoints record's 'points' array via the addPoint method", function() {
           beforeEach(function() {
+            console.log("adding point (4,5)");
             return udpRecord.addPoint(4, 5);
           });
-          return describe("the line's points array", function() {
+          describe("the line's points array", function() {
             var points;
             points = null;
             beforeEach(function() {
               return points = line.get('points');
             });
-            return it("should have the three pairs [0,1], [2,3], and [4,5] (in order)", function() {
+            return it("should have exactly the three pairs [0,1], [2,3], and [4,5] (in that order)", function() {
+              console.log("testing line's points array");
               return expect(points).toEqualPairs([[0, 1], [2, 3], [4, 5]]);
+            });
+          });
+          return describe("the pointset's points array", function() {
+            var points;
+            points = null;
+            beforeEach(function() {
+              return points = pointset.get('points');
+            });
+            return it("should have exactly the Smartgraph.Points [0,1], [2,3], and [4,5] (in that order)", function() {
+              console.log("testing pointset's points array");
+              return expect(points).toEqualPointsDefinedBy([[0, 1], [2, 3], [4, 5]]);
             });
           });
         });
--- apps/smartgraphs/tests/jasmine/integration/src/calculated_data_flow.coffee
+++ apps/smartgraphs/tests/jasmine/integration/src/calculated_data_flow.coffee
@@ -5,23 +5,25 @@ describe "Data flow from UnorderedDataPoints record to GraphableObjects", ->
   udpRecord = store = null
   
   beforeEach ->
+    console.log "making a new store and a new udpRecord"
     store     = SC.Store.create().from(SC.FixturesDataSource.create())
     udpRecord = store.createRecord(Smartgraphs.UnorderedDataPoints,
-      url:    "udp-record"
-    )
-    # not used because Jasmine's toContain works just fine here -- 'expect([[1,2],[3,4]).toContain [1,2]' passes
-
-    @addMatchers(
-      toContainPair: (p) -> return true for a in @actual 
-      toEqualPairs:  (ps) ->
-        return false if not @actual? or ps.length != @actual.length
-        i = 0
-        for a in @actual
-          p = ps[i++]
-          return false if a[0] != p[0] or a[1] != p[1]
-        true
+      url:    "udp-record",
+      points: []
     )
 
+    do =>
+      toMatchUsing = (matcher) ->
+        (pairs) ->
+          return false if @actual?.length isnt pairs.length or typeof @actual isnt 'object'
+          return false for a, i in @actual when not matcher( a, pairs[i] )
+          true
+
+      @addMatchers(
+        toEqualPairs:           toMatchUsing (a, [x, y]) -> a[0]       is x and a[1]       is y
+        toEqualPointsDefinedBy: toMatchUsing (a, [x, y]) -> a.get('x') is x and a.get('y') is y
+      )
+    
   describe "when the source UnorderedDataPoint's 'points' array is empty", ->
     
     dataRepresentation = line = pointset = null
@@ -43,10 +45,9 @@ describe "Data flow from UnorderedDataPoints record to GraphableObjects", ->
         expect( pointset.get('points') ).toBeEmpty()
         
     describe "and the source UnorderedDataPoints record's 'points' array is then populated with [0,1] and [2,3]", ->
-      
-      sourcePoints = [[0,1],[2,3]]
-      
+      sourcePoints = null
       beforeEach ->
+        sourcePoints = [[0,1], [2,3]]
         udpRecord.set('points', sourcePoints)
         
       describe "the line's points array", ->
@@ -54,13 +55,12 @@ describe "Data flow from UnorderedDataPoints record to GraphableObjects", ->
         beforeEach ->
           points = line.get('points')
           
-        it "should have the two pairs [0,1] and [2,3] in its points array", ->
+        it "should have the two pairs [0,1] and [2,3]", ->
           expect(points).toContain [0,1]
           expect(points).toContain [2,3]
           expect(points.length).toEqual 2
   
-        describe "a representative point taken from the line's points array", ->
-          
+        describe "a representative point", ->
           point = null
           beforeEach ->
             point = points[0]
@@ -68,18 +68,35 @@ describe "Data flow from UnorderedDataPoints record to GraphableObjects", ->
           it "should not be the same object as any member of the source points array", ->
             expect(point).not.toBe sourcePoints[0]
             expect(point).not.toBe sourcePoints[1]
-        
-      describe "and a point (4,5) is added to the UnorderedDataPoints record's 'points' array via the addPoint method", ->
       
+      describe "the poinset's points array", ->
+        points = null
         beforeEach ->
+          points = pointset.get('points')
+          
+        it "should have the Smartgraph.Points [0,1] and [2,3]", ->
+          expect(points).toEqualPointsDefinedBy [[0,1], [2,3]]
+      
+      describe "and a point (4,5) is added to the UnorderedDataPoints record's 'points' array via the addPoint method", ->
+        beforeEach ->
+          console.log "adding point (4,5)"
           udpRecord.addPoint(4,5)
         
-        describe "the line's points array", ->
+        describe "the line's points array", ->            
           points = null
           beforeEach ->
             points = line.get('points')
-        
-          it "should have the three pairs [0,1], [2,3], and [4,5] (in order)", ->
+                
+          it "should have exactly the three pairs [0,1], [2,3], and [4,5] (in that order)", ->
+            console.log "testing line's points array"
             expect(points).toEqualPairs [[0,1], [2,3], [4,5]]
-      
-      
+        
+        describe "the pointset's points array", ->
+          points = null
+          beforeEach ->
+            points = pointset.get('points')
+            
+          it "should have exactly the Smartgraph.Points [0,1], [2,3], and [4,5] (in that order)", ->
+            console.log "testing pointset's points array"
+            expect(points).toEqualPointsDefinedBy [[0,1], [2,3], [4,5]]
+        
